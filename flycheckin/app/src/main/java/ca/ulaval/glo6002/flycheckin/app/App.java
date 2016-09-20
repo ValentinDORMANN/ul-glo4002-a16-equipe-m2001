d20c2a4fa11aa255a32ab4c1902b99fbe892f342
Hypolyte Tchoutouo <hypolyte.tchoutouo.1@ulaval.ca>
2016-09-19 20:21:51 -0400

    la premiere version du projet à evoluer

---
+++ flycheckin/app/src/main/java/ca/ulaval/glo6002/flycheckin/app/App.java
@@ -0,0 +1,13 @@
+package ca.ulaval.glo6002.flycheckin.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
