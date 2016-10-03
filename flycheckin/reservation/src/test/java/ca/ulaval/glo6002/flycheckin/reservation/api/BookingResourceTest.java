8dfdc1774899f7bdd65c8f166ebb7dfea8e982c3
Hypolyte Tchoutouo <hypolyte.tchoutouo.1@ulaval.ca>
2016-10-02 22:38:08 -0400

    le mockito

--- flycheckin/reservation/src/test/java/ca/ulaval/glo6002/flycheckin/reservation/api/BookingResourceTest.java
+++ flycheckin/reservation/src/test/java/ca/ulaval/glo6002/flycheckin/reservation/api/BookingResourceTest.java
@@ -2,9 +2,20 @@ package ca.ulaval.glo6002.flycheckin.reservation.api;
 
 import static org.junit.Assert.*;
 
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Test;
 
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class BookingResourceTest {
+	private JsonNode mockJson=mock(JsonNode.class);
 
 	@Test
 	public void test() {
