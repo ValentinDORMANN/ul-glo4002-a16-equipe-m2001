67b276b8ced436e30a00cfefec0bb68c11cadf59
virgil <virgil@debian>
2016-10-02 22:28:30 -0400

    create Service file

---
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo6002/flycheckin/reservation/domain/Service.java
@@ -0,0 +1,26 @@
+package ca.ulaval.glo6002.flycheckin.reservation.domain;
+
+import org.json.JSONObject;
+
+public class Service {
+
+	public Service() {
+
+	}
+
+	public void saveBooking(JSONObject json) {
+
+	}
+
+	public void getBooking() {
+
+	}
+
+	public void changeFlightDate() {
+
+	}
+
+	public void changeFlightNumber() {
+
+	}
+}
