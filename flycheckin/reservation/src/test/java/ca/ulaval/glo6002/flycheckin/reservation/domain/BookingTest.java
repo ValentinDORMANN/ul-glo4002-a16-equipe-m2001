931251a7b69242cab14144ad8584959b623211dd
virgil <virgil@debian>
2016-10-02 22:29:16 -0400

    Test file for Booking Class

--- flycheckin/reservation/src/test/java/ca/ulaval/glo6002/flycheckin/reservation/domain/BookingTest.java
+++ flycheckin/reservation/src/test/java/ca/ulaval/glo6002/flycheckin/reservation/domain/BookingTest.java
@@ -2,13 +2,62 @@ package ca.ulaval.glo6002.flycheckin.reservation.domain;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class BookingTest {
 
+	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	private int RESERVATION_NUMBER = 37353;
+	private String RESERVATION_DATE = "2016-10-03";
+	private String RESERVATION_CONFIRMATION = "A3833";
+	private String FLIGHT_NUMBER = "AC1765";
+	private String FLIGHT_DATE = "2016-10-30";
+	private String PAYMENT_LOCATION = "payments/da3944";
+
+	private Booking booking;
+
+	@Before
+	public void InitializeBooking() throws ParseException {
+		this.booking = new Booking(this.RESERVATION_NUMBER, this.RESERVATION_DATE, this.RESERVATION_CONFIRMATION,
+				this.FLIGHT_NUMBER, this.FLIGHT_DATE, this.PAYMENT_LOCATION);
+
+	}
+
 	@Test
-	public void test() {
-		assertTrue(true);//fail("Not yet implemented");
+	public void lookIfReservationNumberIsTheSame() {
+		assertEquals(this.RESERVATION_NUMBER, this.booking.getReservation_number());
+	}
+
+	@Test
+	public void lookIfReservationDateIsTheSame() throws ParseException {
+		Date date = this.simpleDateFormat.parse(this.RESERVATION_DATE);
+		assertEquals(date, this.booking.getReservation_date());
+	}
+
+	@Test
+	public void lookIfReservationConfirmationIsTheSame() {
+		assertEquals(this.RESERVATION_CONFIRMATION, this.booking.getReservation_confirmation());
+	}
+
+	@Test
+	public void lookIfFlightNumberIsTheSame() {
+		assertEquals(this.FLIGHT_NUMBER, this.booking.getFlight_number());
+	}
+
+	@Test
+	public void lookIfFlightDateIsTheSame() throws ParseException {
+		Date date = this.simpleDateFormat.parse(this.FLIGHT_DATE);
+		assertEquals(date, this.booking.getFlight_date());
+	}
+
+	@Test
+	public void lookIfPaymentLocationIsTheSame() {
+		assertEquals(this.PAYMENT_LOCATION, this.booking.getPayment_location());
 	}
 
 }
