3367a11a264864d3ea08183d29c2c45f07d718fb
ValentinDORMANN <valentin.dormann@gmail.com>
2016-10-06 22:41:58 -0400

    ajout

---
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/api/BoardingResource.java
@@ -0,0 +1,72 @@
+package ca.ulaval.glo4002.flycheckin.boarding.api;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.JSONObject;
+
+import ca.ulaval.glo4002.flycheckin.boarding.domain.BoardingPassenger;
+
+@Path("/checkins")
+@Produces(MediaType.APPLICATION_JSON)
+public class BoardingResource {
+	private JSONObject json;
+
+	public BoardingResource(JSONObject json) {
+		this.json = json;
+	}
+
+	@POST
+	public void createBooking(String message) {
+	}
+
+	public boolean validateJsonBoarding(JSONObject json) {
+		String passengerHash = json.getString("passenger_hash").trim();
+		String agentId = json.getString("by").trim();
+		boolean isOK = validatePassengerHash(passengerHash) && validateAgentId(agentId);
+		return isOK;
+	}
+
+	public boolean validateJsonPassenger(JSONObject json) {
+		String fullname = json.getString("fullname").trim();
+		String passportNumber = json.getString("passeport_number").trim();
+		String hash = json.getString("passenger_number").trim();
+		return (fullname.split(":").length == 2) && validateFullname(fullname) && validatePassportNumber(passportNumber)
+				&& validatePassengerHash(hash);
+	}
+
+	public boolean validatePassengerHash(String passengerHash) {
+		return passengerHash.matches("^[A-Za-z0-9]+:[A-Za-z0-9]+$");// passport:reservationNumber
+	}
+
+	public boolean validateAgentId(String agentId) {
+		return agentId.matches("^[0-9]+$");
+	}
+
+	public boolean validatePassportNumber(String passportNumber) {
+		return passportNumber.matches("^[A-Z0-9]{8,9}$");
+	}
+
+	public boolean validateFullname(String fullname) {
+		return fullname.matches("^[A-Z][a-z]+([-. ][A-Z][a-z]+)*:[A-Z]+$");
+	}
+
+	// passengerHash format "passportNumber:reservationNumber"
+	public JSONObject queryBookingPassenger(String passengerHash) {
+		JSONObject jsonQuery = new JSONObject();
+		jsonQuery.put("passenger_hash", passengerHash);
+		return jsonQuery;
+	}
+
+	public BoardingPassenger receptionBookingPassenger(JSONObject json) {
+		String fullname = json.getString("fullname").trim();
+		String[] fullnameSplited = fullname.split(":");
+		String firstname = fullnameSplited[0];
+		String lastname = fullnameSplited[1];
+		String passportNumber = json.getString("passeport_number").trim();
+		String hash = json.getString("passenger_hash").trim();
+		return new BoardingPassenger(firstname, lastname, passportNumber, hash);
+	}
+}
---
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/BoardingPassenger.java
@@ -0,0 +1,19 @@
+package ca.ulaval.glo4002.flycheckin.boarding.domain;
+
+public class BoardingPassenger {
+	private String firstName;
+	private String lastName;
+	private String passportNumber;
+	private String hash;
+
+	public BoardingPassenger(String firstName, String lastName, String passportNumber, String hash) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.passportNumber = passportNumber;
+		this.hash = hash;
+	}
+
+	public String getHash() {
+		return this.hash;
+	}
+}
