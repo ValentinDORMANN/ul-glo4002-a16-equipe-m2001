4ef92e60f80f7f49b199ac9d4b9e2a09640225c5
Hypolyte Tchoutouo <hypolyte.tchoutouo.1@ulaval.ca>
2016-10-07 12:25:27 -0400

    le test de heartbeatResource.encore à refactorer

--- flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/api/HeartbeatResourceTest.java
+++ flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/api/HeartbeatResourceTest.java
@@ -1,1 +1,27 @@
 package ca.ulaval.glo4002.flycheckin.reservation.api;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Before;
+
+public class HeartbeatResourceTest extends JerseyTest{
+	//private HeartbeatResource heartbeatResource;
+	private  Response response;
+	@Before
+	public Application initialiaze(){
+		return new ResourceConfig(HeartbeatResource.class);
+		
+	}
+	public void givenArequestWhenHeartbeatIsInvokeThenReturnStatus(){
+		response = target("/heartbeat").queryParam("token", "").request().get();
+		
+		assertEquals(response.getStatus(),200);
+		
+	}
+	
+}
