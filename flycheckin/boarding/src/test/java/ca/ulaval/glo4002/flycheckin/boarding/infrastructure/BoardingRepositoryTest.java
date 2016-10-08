fdc48a3ad65f44fee1bef59a19902cbd822ac7fe
chagb <charly.agboton.1@ulaval.ca>
2016-10-07 22:04:11 -0400

    test boarding repository

--- flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/infrastructure/BoardingRepositoryTest.java
+++ flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/infrastructure/BoardingRepositoryTest.java
@@ -1,5 +1,39 @@
 package ca.ulaval.glo4002.flycheckin.boarding.infrastructure;
 
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.ulaval.glo4002.flycheckin.boarding.domain.BoardingPassenger;
+
 public class BoardingRepositoryTest {
+	private final int CHECKIN_DONE_RETURN = 0;
+	BoardingPassenger boardingPassengerTst;
+	private BoardingRepository boardingRepo;
+
+	@Before
+	public void initiateTest() throws ParseException {
+		boardingRepo = new BoardingRepository();
+		boardingPassengerTst = new BoardingPassenger("JOE PATER", "B2F1J4L5", "HACH_PASSENGER");
+		boardingRepo.saveNewBoarding(boardingPassengerTst);
+	}
+
+	@Test
+	public void WhenSavedNewBoardingThenReturnCheckinId() {
+		// Then
+		assertTrue(BoardingRepository.boardingPassengersList.containsValue(boardingPassengerTst));
+	}
+
+	@Test
+	public void WhenSavedNewBoardingTwoTimesThenReturnNull() {
+		// When
+		int checkinDone = boardingRepo.saveNewBoarding(boardingPassengerTst);
+
+		// Then
+		assertEquals(CHECKIN_DONE_RETURN, checkinDone);
+	}
 
 }
