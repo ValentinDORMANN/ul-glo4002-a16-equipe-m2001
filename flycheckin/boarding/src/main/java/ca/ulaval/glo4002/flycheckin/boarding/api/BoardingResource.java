0b7996fb6b8c4fc33a62722f55400814443a192e
ValentinDORMANN <valentin.dormann@gmail.com>
2016-10-13 01:56:26 -0400

    getboarding

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/api/BoardingResource.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/api/BoardingResource.java
@@ -1,7 +1,5 @@
 package ca.ulaval.glo4002.flycheckin.boarding.api;
 
-import java.io.IOException;
-
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,7 +10,6 @@ import javax.ws.rs.core.UriInfo;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -21,7 +18,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import ca.ulaval.glo4002.flycheckin.boarding.domain.Services;
-import javassist.NotFoundException;
 
 @Path("/checkins")
 @Produces(MediaType.APPLICATION_JSON)
@@ -30,71 +26,25 @@ public class BoardingResource {
 	private Services services = new Services();
 	private JSONObject json;
 
-	/*
-	 * @POST public Response getBoarding(@Context UriInfo uriInfo, String
-	 * boardingRequest) throws ClientProtocolException, IOException { this.json
-	 * = new JSONObject(boardingRequest); if (!validateJsonBoarding(json)) {
-	 * return Response.status(400).build(); } else { String passengerHash =
-	 * json.getString("passenger_hash"); String url = RESERVATION_SERVER +
-	 * "/reservations/passengerInfo/" + passengerHash; HttpClient httpClient =
-	 * HttpClientBuilder.create().build(); HttpGet request = new HttpGet(url);
-	 * HttpResponse response = httpClient.execute(request); HttpEntity entity =
-	 * response.getEntity(); JSONObject passenger = new
-	 * JSONObject(EntityUtils.toString(entity, "UTF-8")); if
-	 * (!validatePassengerHash(json.getString("passenger_hash"))) { return
-	 * Response.status(404).build(); } else { if
-	 * (!validateJsonPassenger(passenger) ||
-	 * this.services.createBoarding(passenger) == 0) { return
-	 * Response.status(400).build(); } else { int checkinId =
-	 * this.services.createBoarding(passenger); return
-	 * Response.ok(uriInfo.getBaseUri().toString() + "checkins/" +
-	 * checkinId).build(); } } } }
-	 */
-
 	@POST
-	public Response getBoarding(@Context UriInfo uriInfo, String boardingRequest)
-			throws ClientProtocolException, IOException {
+	public Response getBoarding(@Context UriInfo uriInfo, String boardingRequest) {
 		try {
 			this.json = new JSONObject(boardingRequest);
-			System.out.println("S01: Initial");
-			System.out.println(json.getString("passenger_hash"));
-			if (!validateJsonBoarding(json)) {
-				System.out.println("E02: Invalid Post");
-				throw new JSONException("Invalid Post");
-			}
-			System.out.println("S02: Invalid Post");
 			String passengerHash = json.getString("passenger_hash");
 			String url = RESERVATION_SERVER + "/reservations/passengerInfo/" + passengerHash;
 			HttpClient httpClient = HttpClientBuilder.create().build();
 			HttpGet request = new HttpGet(url);
 			HttpResponse response = httpClient.execute(request);
-			HttpEntity entity = response.getEntity();
-			JSONObject passenger = new JSONObject(EntityUtils.toString(entity, "UTF-8"));
-			System.out.println(passenger.toString());
-			if (!validatePassengerHash(passenger.getString("passenger_hash"))) {
-				System.out.println("E03: Passenger with passengerHash not found");
-				throw new NotFoundException("passenger with passengerHash not found");
-			}
-			System.out.println("S03: Passenger with passengerHash not found");
-			if (!validateJsonPassenger(passenger)) {
-				System.out.println("E04: Information missing");
-				throw new JSONException("Information missing");
-			}
-			System.out.println("S04: Information missing");
-			if (this.services.createBoarding(passenger) == 0) {
-				System.out.println("E05: Boarding already done");
-				throw new JSONException("Boarding already done");
-			}
-			System.out.println("S05: Boarding already done");
-			int checkinId = this.services.createBoarding(passenger);
-			System.out.println("S06: Done");
-			return Response.ok(uriInfo.getBaseUri().toString() + "checkins/" + checkinId).build(); // save
+			HttpEntity entityAnswer = response.getEntity();
+			String response_string = EntityUtils.toString(entityAnswer, "UTF-8");
+			JSONObject passengerJson = new JSONObject(response_string);
+			int checkinId = this.services.createBoarding(passengerJson);
+			System.out.println(checkinId);
+			return Response.ok(uriInfo.getBaseUri().toString() + "checkins/" + checkinId).build();
 		} catch (JSONException e) {
-			// info missing | invalid POST | boarding already done
-			return Response.status(400).build();
-		} catch (NotFoundException e) {
-			// passenger with passengerHash not found
 			return Response.status(404).build();
+		} catch (Exception e) {
+			return Response.status(400).build();
 		}
 	}
 
