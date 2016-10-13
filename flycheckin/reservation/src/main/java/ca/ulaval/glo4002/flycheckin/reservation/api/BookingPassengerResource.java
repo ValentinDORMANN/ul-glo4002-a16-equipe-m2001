83d626b561f5736c83d6ad448f06fd63dc998960
virgil <virgil@debian>
2016-10-12 22:39:00 -0400

    back error for passenger

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/api/BookingPassengerResource.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/api/BookingPassengerResource.java
@@ -1,30 +1,29 @@
 package ca.ulaval.glo4002.flycheckin.reservation.api;
 
-import java.text.ParseException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import ca.ulaval.glo4002.flycheckin.reservation.domain.Services;
-import javassist.NotFoundException;
 
 @Path("/reservations/{reservation_number}")
 public class BookingPassengerResource {
 
 	@GET
-	public Response getNumberResevation(@Context HttpServletRequest request)
-			throws JSONException, ParseException, NumberFormatException, NotFoundException {
+	public Response getNumberResevation(@Context HttpServletRequest request) {
 		String urlArrayWhichContainsNumberReservation[] = request.getRequestURL().toString().split("/");
 		Services service = new Services();
-		JSONObject jsonObject = service.getReservation(Integer
-				.parseInt(urlArrayWhichContainsNumberReservation[urlArrayWhichContainsNumberReservation.length - 1]));
-		return Response.ok(jsonObject.toString()).build();
+		try {
+			JSONObject jsonObject = service.getReservation(Integer.parseInt(
+					urlArrayWhichContainsNumberReservation[urlArrayWhichContainsNumberReservation.length - 1]));
+			return Response.ok(jsonObject.toString()).build();
+		} catch (Exception e) {
+			return Response.status(404).build();
+		}
 	}
 
 }
