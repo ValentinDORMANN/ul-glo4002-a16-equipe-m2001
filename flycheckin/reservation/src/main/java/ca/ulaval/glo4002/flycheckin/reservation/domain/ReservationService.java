bb14f6f000659909dcf044e64b9cf9353ff9cb05
Hypolyte Tchoutouo <hypolyte.tchoutouo.1@ulaval.ca>
2016-10-22 19:05:22 -0400

    Service

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/ReservationService.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/ReservationService.java
@@ -1,5 +1,12 @@
 package ca.ulaval.glo4002.flycheckin.reservation.domain;
 
+import ca.ulaval.glo4002.flycheckin.reservation.api.DTO.ReservationDto;
+
 public class ReservationService {
+
+	public Reservation createReservation(ReservationDto reservationDto) {
+		
+		return new Reservation(reservationDto.reservation_number,reservationDto.reservation_date,reservationDto.reservation_confirmation,reservationDto.flight_number,reservationDto.flight_date,reservationDto.payment_location,reservationDto.passengers);;
+	}
 
 }
--- flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/domain/ReservationServiceTest.java
+++ flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/domain/ReservationServiceTest.java
@@ -1,5 +1,27 @@
 package ca.ulaval.glo4002.flycheckin.reservation.domain;
 
-public class ReservationServiceTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import ca.ulaval.glo4002.flycheckin.reservation.api.DTO.ReservationDto;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
+
+
+public class ReservationServiceTest {
+	private ReservationDto reservationDto = new ReservationDto();
+	private Reservation reservationmock=mock(Reservation.class);
+	private ReservationService service;
+	@Before
+	public void initialize(){
+		service = new ReservationService();
+		
+	}
+
+	@Test
+	public void givenDTOReservationWhenCreatingReservationThenVerySave(){
+		willReturn(reservationmock).given(service).createReservation(reservationDto);
+		verify(reservationmock,times(1)).save();
+	}
 }
