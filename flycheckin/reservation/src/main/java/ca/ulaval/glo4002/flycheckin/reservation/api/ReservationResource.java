f31a445496dd9a7ce6014c77445fb94548d57621
virgil <virgil@debian>
2016-10-22 15:48:46 -0400

    begin ReservationResource

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/api/ReservationResource.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/api/ReservationResource.java
@@ -1,5 +1,27 @@
 package ca.ulaval.glo4002.flycheckin.reservation.api;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+import ca.ulaval.glo4002.flycheckin.reservation.api.DTO.*;
+import ca.ulaval.glo4002.flycheckin.reservation.domain.*;
+
+@Path("/events/reservation-created")
+@Produces(MediaType.APPLICATION_JSON)
 public class ReservationResource {
 
+  private ReservationService reservationService;
+
+  public ReservationResource() {
+    this.reservationService = new ReservationService();
+  }
+
+  public ReservationResource(ReservationService reservationService) {
+    this.reservationService = reservationService;
+  }
+
+  @POST
+  public Response createReserversation(ReservationDto reservationDto) {
+    return Response.status(201).build();
+  }
 }
