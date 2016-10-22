48b189fcbbbc9b00bf3309d952b08cb17cbe3b7b
Hypolyte Tchoutouo <hypolyte.tchoutouo.1@ulaval.ca>
2016-10-22 19:49:24 -0400

    service refactor

--- flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/domain/ReservationServiceTest.java
+++ flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/domain/ReservationServiceTest.java
@@ -11,17 +11,19 @@ import static org.mockito.BDDMockito.*;
 
 public class ReservationServiceTest {
 	private ReservationDto reservationDto = new ReservationDto();
-	private Reservation reservationmock=mock(Reservation.class);
+	private Reservation mockreservation=mock(Reservation.class);
 	private ReservationService service;
 	@Before
 	public void initialize(){
 		service = new ReservationService();
-		
 	}
 
 	@Test
 	public void givenDTOReservationWhenCreatingReservationThenVerySave(){
-		willReturn(reservationmock).given(service).createReservation(reservationDto);
-		verify(reservationmock,times(1)).save();
+		willReturn(mockreservation).given(service).createReservation(reservationDto);
+		
+		mockreservation=service.createReservation(reservationDto);
+		
+		verify(mockreservation,times(1)).save();
 	}
 }
