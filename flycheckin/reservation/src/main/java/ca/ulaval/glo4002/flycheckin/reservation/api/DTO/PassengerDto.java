97f13ceb9031543c2a3c6aab7d852ce6d93da00e
ValentinDORMANN <valentin.dormann@gmail.com>
2016-10-22 15:42:01 -0400

    ajout DTO

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/api/DTO/PassengerDto.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/api/DTO/PassengerDto.java
@@ -2,4 +2,9 @@ package ca.ulaval.glo4002.flycheckin.reservation.api.DTO;
 
 public class PassengerDto {
 
+	public String first_name;
+	public String last_name;
+	public int age;
+	public String passport_number;
+	public String seat_class;
 }
