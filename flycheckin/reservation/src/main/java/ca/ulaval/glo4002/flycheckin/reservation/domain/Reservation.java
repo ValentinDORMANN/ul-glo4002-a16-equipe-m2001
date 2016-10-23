bddee5e9e83327ed3d3ff8cb1ede5195ec77d47d
virgil <virgil@debian>
2016-10-22 23:45:21 -0400

    add getters

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/Reservation.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/Reservation.java
@@ -1,12 +1,10 @@
 package ca.ulaval.glo4002.flycheckin.reservation.domain;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
-import ca.ulaval.glo4002.flycheckin.reservation.api.DTO.ReservationDto;
-import ca.ulaval.glo4002.flycheckin.reservation.exception.IllegalArgumentReservationException;
-import ca.ulaval.glo4002.flycheckin.reservation.persistence.ReservationInMemory;
+import ca.ulaval.glo4002.flycheckin.reservation.api.DTO.*;
+import ca.ulaval.glo4002.flycheckin.reservation.exception.*;
+import ca.ulaval.glo4002.flycheckin.reservation.persistence.*;
 
 public class Reservation {
   private int reservationNumber;
@@ -37,15 +35,43 @@ public class Reservation {
     createReservation();
   }
 
-  public int getReservationNumber() {
-    return reservationNumber;
-  }
-
   private void createReservation() throws IllegalArgumentReservationException {
     reservationInMemory.saveNewReservation(this);
   }
 
   public Reservation readReservationByNumber(int reservationNumber) {
     return reservationInMemory.getReservationByNumber(reservationNumber);
+  }
+
+  public Date getReservationDate() {
+    return reservationDate;
+  }
+
+  public String getReservationConfirmation() {
+    return reservationConfirmation;
+  }
+
+  public String getFlightNumber() {
+    return flightNumber;
+  }
+
+  public Date getFlightDate() {
+    return flightDate;
+  }
+
+  public String getPaymentLocation() {
+    return paymentLocation;
+  }
+
+  public List<Passenger> getPassengers() {
+    return passengers;
+  }
+
+  public ReservationInMemory getReservationInMemory() {
+    return reservationInMemory;
+  }
+
+  public int getReservationNumber() {
+    return reservationNumber;
   }
 }
