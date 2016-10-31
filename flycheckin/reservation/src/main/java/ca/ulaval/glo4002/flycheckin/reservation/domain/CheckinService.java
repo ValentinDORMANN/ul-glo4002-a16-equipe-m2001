7704517c5fedc28972f201731e0456b8c821006f
ValentinDORMANN <valentin.dormann@gmail.com>
2016-10-31 19:26:31 -0400

    checkingservice progression

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/CheckinService.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/CheckinService.java
@@ -1,6 +1,18 @@
 package ca.ulaval.glo4002.flycheckin.reservation.domain;
 
-public class Service {
-	
+import ca.ulaval.glo4002.flycheckin.reservation.api.dto.CheckinDto;
+import ca.ulaval.glo4002.flycheckin.reservation.exception.NotFoundReservationException;
+import ca.ulaval.glo4002.flycheckin.reservation.persistence.CheckinInMemory;
+
+public class CheckinService {
+
+  private CheckinInMemory checkinInMemory;
+
+  public CheckinService() {
+    this.checkinInMemory = new CheckinInMemory();
+  }
   
+  public int saveCheckin(CheckinDto checkindto) throws NotFoundReservationException {
+    return checkinInMemory.doPassengerCheckin(checkindto.passenger_hash);
+  }
 }
