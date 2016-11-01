4c7531e30c4186cc3c93dfaf5a710692023878ca
ValentinDORMANN <valentin.dormann@gmail.com>
2016-10-31 20:58:21 -0400

    add method in reservationInMemory + NotFoundPassengerException

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/persistence/ReservationInMemory.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/persistence/ReservationInMemory.java
@@ -1,10 +1,15 @@
 package ca.ulaval.glo4002.flycheckin.reservation.persistence;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Iterator;
 
+import ca.ulaval.glo4002.flycheckin.reservation.domain.Passenger;
 import ca.ulaval.glo4002.flycheckin.reservation.domain.Reservation;
 import ca.ulaval.glo4002.flycheckin.reservation.exception.IllegalArgumentReservationException;
+import ca.ulaval.glo4002.flycheckin.reservation.exception.NotFoundPassengerException;
 import ca.ulaval.glo4002.flycheckin.reservation.exception.NotFoundReservationException;
 
 public class ReservationInMemory {
@@ -25,5 +30,29 @@ public class ReservationInMemory {
     else
       return reservationList.get(reservationNumber);
   }
-
+  
+  private Passenger getPassengerByPassengerHashInReservation(int reservationNumber, String passengerHash){
+    List<Passenger> passengers = reservationList.get(reservationNumber).getPassengers();
+    Passenger passengerFound = null;
+    for(int i = 0; i < passengers.size(); i++){
+      if(passengers.get(i).getPassengerHash() == passengerHash){
+        passengerFound = passengers.get(i);
+      }
+    }
+    if(passengerFound == null){
+      throw new NotFoundPassengerException("Passenger with " + passengerHash + " not found");
+    }
+    return passengerFound;
+  }
+  
+  public Passenger getPassengerByPassengerHash(String passengerHash){
+    Passenger passengerFound = null;
+    Set<Integer> reservationNumbers = reservationList.keySet();
+    Iterator<Integer> iterator = reservationNumbers.iterator();
+    while(iterator.hasNext()){
+      int reservationNumber = iterator.next();
+      passengerFound = getPassengerByPassengerHashInReservation(reservationNumber, passengerHash);
+    }
+    return passengerFound;
+  }
 }
