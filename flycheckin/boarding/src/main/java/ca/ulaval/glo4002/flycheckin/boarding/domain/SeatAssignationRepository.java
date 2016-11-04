0f3fc2f01ac4e9da9b7a3940309483ea6862fa38
chagb <charly.agboton.1@ulaval.ca>
2016-11-03 22:54:55 -0400

    boarding module structure main and test

---
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/SeatAssignationRepository.java
@@ -0,0 +1,5 @@
+package ca.ulaval.glo4002.flycheckin.boarding.domain;
+
+public interface SeatAssignationRepository {
+
+}
---
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/persistence/InMemorySeatAssignation.java
@@ -0,0 +1,7 @@
+package ca.ulaval.glo4002.flycheckin.boarding.persistence;
+
+import ca.ulaval.glo4002.flycheckin.boarding.domain.SeatAssignationRepository;
+
+public class InMemorySeatAssignation implements SeatAssignationRepository {
+
+}
