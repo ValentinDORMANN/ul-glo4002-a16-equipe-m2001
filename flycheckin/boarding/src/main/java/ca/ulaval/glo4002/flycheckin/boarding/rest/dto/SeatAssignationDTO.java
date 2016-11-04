46ac96233f86df5d16574c6fadaa57f54b78af7a
virgil <virgil@debian>
2016-11-03 22:19:35 -0400

    instance new server

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/rest/dto/SeatAssignationDTO.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/rest/dto/SeatAssignationDTO.java
@@ -1,5 +1,11 @@
 package ca.ulaval.glo4002.flycheckin.boarding.rest.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class SeatAssignationDTO {
 
+  public String passenger_hash;
+  public String mode;
+  public String seat;
 }
