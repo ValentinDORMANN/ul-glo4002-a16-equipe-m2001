e03614344af7f5ac303c9ff3f605706bad0818a7
virgil <virgil@debian>
2016-11-06 19:10:56 -0500

    refactor Dto

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/rest/dto/LuggageDto.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/rest/dto/LuggageDto.java
@@ -2,10 +2,12 @@ package ca.ulaval.glo4002.flycheckin.boarding.rest.dto;
 
 public class LuggageDto {
 
-	public int dimension;
-	public String dimension_unit;
-	public int weight;
-	public String weight_unit;
-	public String type;
+  public int linear_dimension;
+  public String linear_dimension_unit;
+  public int weight;
+  public String weight_unit;
+  public String type;
+  public boolean allowed;
+  public String refusation_reason;
 
 }
