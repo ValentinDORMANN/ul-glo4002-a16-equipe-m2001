6ae04fb114e8f5445f93a86f3a0a774af88b8d38
virgil <virgil@debian>
2016-11-06 19:18:23 -0500

    add Resources

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/Luggage.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/Luggage.java
@@ -12,24 +12,24 @@ public abstract class Luggage {
   private int weightInPound;
   private String luggageHash;
   private String type;
-  
+
   public Luggage(LuggageDto luggageDto) throws IllegalArgumentException {
     this.luggageHash = UUID.randomUUID().toString();
-    this.dimensionInInch = convertDimensionToInchUnit(luggageDto.dimension, luggageDto.dimension_unit);
+    this.dimensionInInch = convertDimensionToInchUnit(luggageDto.linear_dimension, luggageDto.linear_dimension_unit);
     this.weightInPound = convertWeightToPoundUnit(luggageDto.weight, luggageDto.weight_unit);
     this.type = luggageDto.type;
     this.luggageHash = UUID.randomUUID().toString();
   }
 
   private int convertDimensionToInchUnit(int dimension, String dimmensionUnit) {
-    if(dimmensionUnit.equals("po"))
+    if (dimmensionUnit.equals("po"))
       return dimension;
     else
       return (int) Math.floor(dimension * DIMENSION_CONVERSION_RATE);
   }
 
   private int convertWeightToPoundUnit(int weight, String weightUnit) {
-    if(weightUnit.equals("lbs"))
+    if (weightUnit.equals("lbs"))
       return weight;
     else
       return (int) Math.floor(weight * WEIGHT_CONVERSION_RATE);
@@ -38,11 +38,11 @@ public abstract class Luggage {
   public boolean isType(String type) {
     return type.equals(type);
   }
-  
+
   public int getDimensionInInch() {
     return dimensionInInch;
   }
-  
+
   public int getWeightInPound() {
     return weightInPound;
   }
--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/rest/LuggageCheckinResource.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/rest/LuggageCheckinResource.java
@@ -1,5 +1,18 @@
 package ca.ulaval.glo4002.flycheckin.boarding.rest;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import ca.ulaval.glo4002.flycheckin.boarding.rest.dto.LuggageDto;
+
+@Path("")
 public class LuggageCheckinResource {
 
+  @POST
+  @Path("/passengers/{passenger_hash}/baggages")
+  public Response checkinLuggage(@PathParam("passneger_hash") String passengerHash, LuggageDto luggageDto) {
+    return Response.ok().build();
+  }
 }
