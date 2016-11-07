7b56ee97a4fc70db02c3cd1222bc6b8e5188317c
khalloud siham <siham.khalloud.1@ulaval.com>
2016-11-06 21:34:02 -0500

    Enlever le type dans CheckedLuggage.java

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/CheckedLuggage.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/CheckedLuggage.java
@@ -6,11 +6,9 @@ public class CheckedLuggage extends Luggage {
 
   private static final int WEIGHT_LIMIT = 50;
   private static final int DIMENSION_LIMIT = 62;
-  private static final String TYPE_CHECKED = "checked";
 
   public CheckedLuggage(LuggageDto luggageDto) {
     super(luggageDto);
-    luggageDto.type = TYPE_CHECKED;
   }
 
   public boolean isAllowed() {
