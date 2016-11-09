19caeea41947214822409f330637abb2915704c4
chagb <charly.agboton.1@ulaval.ca>
2016-11-08 22:44:06 -0500

    Throw exception reason depending of checkLuggageAllowable

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/CheckedLuggage.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/CheckedLuggage.java
@@ -1,9 +1,12 @@
 package ca.ulaval.glo4002.flycheckin.boarding.domain;
 
+import ca.ulaval.glo4002.flycheckin.boarding.exception.ExcededCheckedLuggageException;
 import ca.ulaval.glo4002.flycheckin.boarding.rest.dto.LuggageDto;
 
 public class CheckedLuggage extends Luggage {
 
+  private static final String LUGGAGE_DIMENSION_NOT_ALLOWED = "The size of luggage is over boundary";
+  private static final String LUGGAGE_WEIGHT_NOT_ALLOWED = "The weight of luggage is over boundary";
   private static final int WEIGHT_LIMIT = 23;
   private static final int DIMENSION_LIMIT = 158;
 
@@ -11,15 +14,18 @@ public class CheckedLuggage extends Luggage {
     super(luggageDto);
   }
 
-  public boolean isAllowed() {
-    return isWeightAllowed() && isDimensionAllowed();
+  public void checkLuggageAllowable() throws ExcededCheckedLuggageException {
+    checkLuggageWeight();
+    checkLuggageDimension();
   }
 
-  private boolean isWeightAllowed() {
-    return getWeightInKg() <= WEIGHT_LIMIT;
+  private void checkLuggageWeight() {
+    if (getWeightInKg() > WEIGHT_LIMIT)
+      throw new ExcededCheckedLuggageException(LUGGAGE_WEIGHT_NOT_ALLOWED);
   }
 
-  private boolean isDimensionAllowed() {
-    return getDimensionInCm() <= DIMENSION_LIMIT;
+  private void checkLuggageDimension() {
+    if (getDimensionInCm() > DIMENSION_LIMIT)
+      throw new ExcededCheckedLuggageException(LUGGAGE_DIMENSION_NOT_ALLOWED);
   }
 }
