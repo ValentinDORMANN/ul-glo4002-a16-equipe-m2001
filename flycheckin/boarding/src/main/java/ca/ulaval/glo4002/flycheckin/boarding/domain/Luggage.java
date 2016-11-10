ed74c946da67b452aac42a3244657eaa11ca51af
ValentinDORMANN <valentin.dormann@gmail.com>
2016-11-10 02:34:16 -0500

    elimination de luggageDto in domain

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/Luggage.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/Luggage.java
@@ -7,6 +7,7 @@ import ca.ulaval.glo4002.flycheckin.boarding.rest.dto.LuggageDto;
 
 public abstract class Luggage {
 
+  private static final String TYPE = "";
   private static final double DIMENSION_CONVERSION_RATE = (double) 158 / 62;
   private static final double WEIGHT_CONVERSION_RATE = (double) 23 / 50;
   private static final String CM = "cm";
@@ -16,14 +17,12 @@ public abstract class Luggage {
   private int dimensionInCm;
   private int weightInKg;
   private String luggageHash;
-  private String type;
   private double price;
 
-  public Luggage(LuggageDto luggageDto) throws IllegalArgumentException {
-    this.dimensionInCm = convertDimensionToCmUnit(luggageDto.linear_dimension, luggageDto.linear_dimension_unit);
-    this.weightInKg = convertWeightToKgUnit(luggageDto.weight, luggageDto.weight_unit);
+  public Luggage(int linearDimension, String linearDimensionUnit, int weight, String weightUnit) throws IllegalArgumentException {
+    this.dimensionInCm = convertDimensionToCmUnit(linearDimension, linearDimensionUnit);
+    this.weightInKg = convertWeightToKgUnit(weight, weightUnit);
     this.luggageHash = UUID.randomUUID().toString();
-    this.type = luggageDto.type;
     price = 0;
   }
 
@@ -52,7 +51,7 @@ public abstract class Luggage {
   }
 
   public boolean isType(String type) {
-    return this.type.equals(type);
+    return TYPE.equals(type);
   }
 
   public int getDimensionInCm() {
