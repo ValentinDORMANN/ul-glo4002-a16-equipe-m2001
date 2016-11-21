102fc6cc8de428713b0a4a6f7899098dc4a9ec80
virgil <virgil@debian>
2016-11-20 19:48:55 -0500

    remove lbs and po in luggage test

--- flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/domain/luggage/CheckedLuggageTest.java
+++ flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/domain/luggage/CheckedLuggageTest.java
@@ -4,24 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import ca.ulaval.glo4002.flycheckin.boarding.domain.luggage.CheckedLuggage;
 import ca.ulaval.glo4002.flycheckin.boarding.exception.ExcededLuggageException;
 
 public class CheckedLuggageTest {
   private static final int WEIGHT_LIMIT_KG = 23;
   private static final int OVER_WEIGHT_KG = 24;
-  private static final int OVER_WEIGHT_LBS = 51;
   private static final int ALLOWED_WEIGHT_KG = 22;
-  private static final int ALLOWED_WEIGHT_LBS = 49;
   private static final int OVER_DIMENSION_CM = 159;
   private static final int DIMENSION_LIMIT_CM = 158;
-  private static final int OVER_DIMENSION_PO = 63;
   private static final int ALLOWED_DIMENSION_CM = 157;
-  private static final int ALLOWED_DIMENSION_PO = 61;
   private static final String DIMENSION_UNIT_IN_CM = "cm";
   private static final String WEIGHT_UNIT_IN_KG = "kg";
-  private static final String WEIGHT_UNIT_IN_LBS = "lbs";
-  private static final String DIMENSION_UNIT_IN_PO = "po";
   private static final String LUGGAGE_TYPE = "checked";
   private static final String OTHER_LUGGAGE_TYPE = "other";
   private CheckedLuggage checkedLuggage;
@@ -36,22 +29,6 @@ public class CheckedLuggageTest {
   @Test(expected = ExcededLuggageException.class)
   public void givenLuggageDimensionInCmOverLimitWhenCheckLuggageAllowableThenThrowException() {
     checkedLuggage = new CheckedLuggage(OVER_DIMENSION_CM, DIMENSION_UNIT_IN_CM, ALLOWED_WEIGHT_KG, WEIGHT_UNIT_IN_KG);
-
-    checkedLuggage.checkLuggageAllowable();
-  }
-
-  @Test(expected = ExcededLuggageException.class)
-  public void givenLuggageWeightInLbsOverLimitWhenCheckLuggageAllowableThenThrowException() {
-    checkedLuggage = new CheckedLuggage(ALLOWED_DIMENSION_PO, DIMENSION_UNIT_IN_PO, OVER_WEIGHT_LBS,
-        WEIGHT_UNIT_IN_LBS);
-
-    checkedLuggage.checkLuggageAllowable();
-  }
-
-  @Test(expected = ExcededLuggageException.class)
-  public void givenLuggageDimensionInPoOverLimitWhenCheckLuggageAllowableThenThrowException() {
-    checkedLuggage = new CheckedLuggage(OVER_DIMENSION_PO, DIMENSION_UNIT_IN_PO, ALLOWED_WEIGHT_LBS,
-        WEIGHT_UNIT_IN_LBS);
 
     checkedLuggage.checkLuggageAllowable();
   }
