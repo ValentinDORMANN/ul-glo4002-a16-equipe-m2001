b56aa5ee89a8a3fca27c9274bf62d1ce2d086e87
ValentinDORMANN <valentin.dormann@gmail.com>
2016-11-27 18:34:10 -0500

    RegularPassengerTest

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/passenger/RegularPassenger.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/passenger/RegularPassenger.java
@@ -3,6 +3,7 @@ package ca.ulaval.glo4002.flycheckin.boarding.domain.passenger;
 import java.util.Date;
 
 import ca.ulaval.glo4002.flycheckin.boarding.domain.luggage.Luggage;
+import ca.ulaval.glo4002.flycheckin.boarding.exception.NotAllowableLuggageException;
 
 public class RegularPassenger extends Passenger{
   
@@ -11,6 +12,7 @@ public class RegularPassenger extends Passenger{
   private static final double CARRY_ON_LUGGAGE_PRICE = 30;
   private static final int CHECKED_LUGGAGE_WEIGHT_MAX_KG = 23;
   private static final int CHECKED_LUGGAGE_DIMENSION_MAX_CM = 158;
+  private static final double FREE = 0;
 
   public RegularPassenger(String flightNumber, Date flightDate, String passengerHash, String seatClass, boolean isVip) {
     super(flightNumber, flightDate, passengerHash, seatClass, isVip);
@@ -18,20 +20,29 @@ public class RegularPassenger extends Passenger{
 
   @Override
   protected double calculateLuggagePrice(Luggage luggage) {
-    if (luggage.isType(CHECKED_LUGGAGE_TYPE))
-      return EXCEEDED_CHECKED_LUGGAGE_PRICE;
-    else
+    if (luggage.isType(CHECKED_LUGGAGE_TYPE)) {
+      if (countTypeLuggageAssigned(CHECKED_LUGGAGE_TYPE) < FREE_CHECKED_LUGGAGE_LIMIT)
+        return FREE;
+      else
+        return EXCEEDED_CHECKED_LUGGAGE_PRICE;
+    } else
       return CARRY_ON_LUGGAGE_PRICE;
   }
 
   @Override
-  protected void verifyLuggageDimensionAllowable(Luggage luggage) {
-    luggage.verifyAllowableDimension(CHECKED_LUGGAGE_DIMENSION_MAX_CM);
+  protected void verifyLuggageDimensionAllowable(Luggage luggage) throws NotAllowableLuggageException{
+    if(luggage.isType(CHECKED_LUGGAGE_TYPE))
+      luggage.verifyAllowableDimension(CHECKED_LUGGAGE_DIMENSION_MAX_CM);
+    else
+      luggage.checkLuggageAllowable();
   }
 
   @Override
-  protected void verifyLuggageWeightAllowable(Luggage luggage) {
-    luggage.verifyAllowableWeight(CHECKED_LUGGAGE_WEIGHT_MAX_KG);
+  protected void verifyLuggageWeightAllowable(Luggage luggage) throws NotAllowableLuggageException{
+    if(luggage.isType(CHECKED_LUGGAGE_TYPE))
+      luggage.verifyAllowableWeight(CHECKED_LUGGAGE_WEIGHT_MAX_KG);
+    else
+      luggage.checkLuggageAllowable();
   }
 
 }
--- flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/domain/passenger/RegularPassengerTest.java
+++ flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/domain/passenger/RegularPassengerTest.java
@@ -5,18 +5,33 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.Date;
+import java.util.List;
+
 import ca.ulaval.glo4002.flycheckin.boarding.domain.luggage.CarryOnLuggage;
 import ca.ulaval.glo4002.flycheckin.boarding.domain.luggage.CheckedLuggage;
+import ca.ulaval.glo4002.flycheckin.boarding.domain.luggage.Luggage;
 import ca.ulaval.glo4002.flycheckin.boarding.exception.NotAllowableLuggageException;
 
 public class RegularPassengerTest {
 
   private static final double CHECKED_LUGGAGE_PRICE = 50;
   private static final double CARRY_ON_LUGGAGE_PRICE = 30;
-  private static final double DELTA = 0.01;
+  private static final double FREE_LUGGAGE_PRICE = 0;
   private static final String CHECKED_LUGGAGE_TYPE = "checked";
+  private static final String CARRY_ON_LUGGAGE_TYPE = "carry-on";
   private static final int CHECKED_LUGGAGE_DIMENSION_MAX_CM = 158;
   private static final int CHECKED_LUGGAGE_WEIGHT_MAX_KG = 23;
+  
+  private static final String FLIGHT_NUMBER = "AAAA";
+  private static final Date FLIGHT_DATE = new Date();
+  private static final String HASH = "hash";
+  private static final String ECONOMY = "economy";
+  private static final boolean VIP_STATUS = false;
+  private static final double DELTA = 0.01;
   
   private CarryOnLuggage carryOnLuggageMock;
   private CheckedLuggage checkedLuggageMock;
@@ -24,10 +39,10 @@ public class RegularPassengerTest {
   private RegularPassenger regularPassenger;
   
   @Before
-  public void initiateTest(){
+  public void initiateTest() {
     carryOnLuggageMock = mock(CarryOnLuggage.class);
     checkedLuggageMock = mock(CheckedLuggage.class);
-    regularPassenger = new RegularPassenger(null, null, null, null, false);
+    regularPassenger = new RegularPassenger(FLIGHT_NUMBER, FLIGHT_DATE, HASH, ECONOMY, VIP_STATUS);
   }
   
   @Test
@@ -40,39 +55,121 @@ public class RegularPassengerTest {
   }
   
   @Test
-  public void givenCheckedLuggageWhenCalculateLuggagePriceThenReturnChechedLuggagePrice() {
+  public void givenCheckedLuggageWhenCalculateLuggagePriceThenReturnCheckedLuggagePrice() {
     given(checkedLuggageMock.isType(CHECKED_LUGGAGE_TYPE)).willReturn(true);
     
     double price = regularPassenger.calculateLuggagePrice(checkedLuggageMock);
     
-    assertEquals(CHECKED_LUGGAGE_PRICE, price, DELTA);
+    assertEquals(0, price, DELTA);
+  }
+  
+  @Test
+  public void givenCheckedLuggageWhenVerifyLuggageDimensionAllowableThenVerify() {
+    given(checkedLuggageMock.isType(CHECKED_LUGGAGE_TYPE)).willReturn(true);
+    
+    regularPassenger.verifyLuggageDimensionAllowable(checkedLuggageMock);
+    
+    verify(checkedLuggageMock).verifyAllowableDimension(CHECKED_LUGGAGE_DIMENSION_MAX_CM);
   }
   
   @Test
   public void givenCarryOnLuggageWhenVerifyLuggageDimensionAllowableThenVerify() {  
     regularPassenger.verifyLuggageDimensionAllowable(carryOnLuggageMock);
     
-    verify(carryOnLuggageMock).verifyAllowableDimension(CHECKED_LUGGAGE_DIMENSION_MAX_CM);
+    verify(carryOnLuggageMock).checkLuggageAllowable();
   }
   
   @Test(expected = NotAllowableLuggageException.class)
   public void givenOverDimensionCarryOnLuggageWhenVerifyLuggageDimensionAllowableThenThrowException() {
-    willThrow(new NotAllowableLuggageException()).given(carryOnLuggageMock).verifyAllowableDimension(CHECKED_LUGGAGE_DIMENSION_MAX_CM);
+    willThrow(new NotAllowableLuggageException()).given(carryOnLuggageMock).checkLuggageAllowable();
   
     regularPassenger.verifyLuggageDimensionAllowable(carryOnLuggageMock);
+  }
+  
+  @Test(expected = NotAllowableLuggageException.class)
+  public void givenOverDimensionCheckedLuggageWhenVerifyLuggageDimensionAllowableThenThrowException() {
+    given(checkedLuggageMock.isType(CHECKED_LUGGAGE_TYPE)).willReturn(true);
+    
+    willThrow(new NotAllowableLuggageException()).given(checkedLuggageMock).verifyAllowableDimension(CHECKED_LUGGAGE_DIMENSION_MAX_CM);
+  
+    regularPassenger.verifyLuggageDimensionAllowable(checkedLuggageMock);
   }
   
   @Test
   public void givenCarryOnLuggageWhenVerifyLuggageWeightAllowableThenVerify() {
     regularPassenger.verifyLuggageWeightAllowable(carryOnLuggageMock);
     
-    verify(carryOnLuggageMock).verifyAllowableWeight(CHECKED_LUGGAGE_WEIGHT_MAX_KG);
+    verify(carryOnLuggageMock).checkLuggageAllowable();
+  }
+  
+  @Test
+  public void givenCheckedLuggageWhenVerifyLuggageWeightAllowableThenVerify() {
+    given(checkedLuggageMock.isType(CHECKED_LUGGAGE_TYPE)).willReturn(true);
+    
+    regularPassenger.verifyLuggageWeightAllowable(checkedLuggageMock);
+    
+    verify(checkedLuggageMock).verifyAllowableWeight(CHECKED_LUGGAGE_WEIGHT_MAX_KG);
   }
   
   @Test(expected = NotAllowableLuggageException.class)
   public void givenOverWeightCarryOnLuggageWhenVerifyLuggageAllowableThenThrowException() {
-    willThrow(new NotAllowableLuggageException()).given(carryOnLuggageMock).verifyAllowableWeight(CHECKED_LUGGAGE_WEIGHT_MAX_KG);
+    willThrow(new NotAllowableLuggageException()).given(carryOnLuggageMock).checkLuggageAllowable();
   
     regularPassenger.verifyLuggageWeightAllowable(carryOnLuggageMock);
+  }
+  
+  @Test(expected = NotAllowableLuggageException.class)
+  public void givenOverWeightCheckedLuggageWhenVerifyLuggageAllowableThenThrowException() {
+    willThrow(new NotAllowableLuggageException()).given(checkedLuggageMock).checkLuggageAllowable();
+  
+    regularPassenger.verifyLuggageWeightAllowable(checkedLuggageMock);
+  }
+
+  @Test
+  public void whenAddFirstCarryOnLuggageAddItAndVerifyPrice() {
+    regularPassenger.addLuggage(carryOnLuggageMock);
+    
+    verify(carryOnLuggageMock, times(1)).setPrice(CARRY_ON_LUGGAGE_PRICE);
+    List<Luggage> luggages = regularPassenger.getLuggages();
+    assertEquals(1, luggages.size());
+  }
+  
+  @Test(expected = NotAllowableLuggageException.class)
+  public void whenAddMoreThanOneCarryOnLuggageThenThrownException() {
+    given(carryOnLuggageMock.isType(CARRY_ON_LUGGAGE_TYPE)).willReturn(true);
+    
+    regularPassenger.addLuggage(carryOnLuggageMock);
+    regularPassenger.addLuggage(carryOnLuggageMock);
+  }
+  
+  @Test
+  public void whenAddFisrtCheckedLuggageThenAdditAndVerifyPrice() {
+    given(checkedLuggageMock.isType(CHECKED_LUGGAGE_TYPE)).willReturn(true);
+    
+    regularPassenger.addLuggage(checkedLuggageMock);
+    
+    verify(checkedLuggageMock, times(1)).setPrice(FREE_LUGGAGE_PRICE);
+  }
+  
+  @Test
+  public void whenAddThreeCheckedLuggageThenAddThemAndVerifyPrice() {
+    given(checkedLuggageMock.isType(CHECKED_LUGGAGE_TYPE)).willReturn(true);
+
+    regularPassenger.addLuggage(checkedLuggageMock);
+    regularPassenger.addLuggage(checkedLuggageMock);
+    regularPassenger.addLuggage(checkedLuggageMock);
+
+    verify(checkedLuggageMock, times(1)).setPrice(FREE_LUGGAGE_PRICE);
+    verify(checkedLuggageMock, times(2)).setPrice(CHECKED_LUGGAGE_PRICE);
+  }
+  
+  @Test(expected = NotAllowableLuggageException.class)
+  public void whenAddMoreThanThreeCheckedLuggageThenThrownException() {
+    given(checkedLuggageMock.isType(CHECKED_LUGGAGE_TYPE)).willReturn(true);
+    
+    regularPassenger.addLuggage(checkedLuggageMock);
+    regularPassenger.addLuggage(checkedLuggageMock);
+    regularPassenger.addLuggage(checkedLuggageMock);
+    regularPassenger.addLuggage(checkedLuggageMock);
   }
 }
