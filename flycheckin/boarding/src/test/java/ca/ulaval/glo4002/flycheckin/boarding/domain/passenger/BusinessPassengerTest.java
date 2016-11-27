7f1cb2a45c80a0188a1edf45d0ccd097cd0cfb3b
virgil <virgil@debian>
2016-11-26 19:39:59 -0500

    change DTO for isVip

---
+++ flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/domain/passenger/BusinessPassengerTest.java
@@ -0,0 +1,17 @@
+package ca.ulaval.glo4002.flycheckin.boarding.domain.passenger;
+
+import java.util.Date;
+
+import org.junit.Before;
+
+public class BusinessPassengerTest {
+
+  private static final String FLIGHT_NUMBER = "AAAA";
+  private static final Date FLIGHT_DATE = new Date();
+  private BusinessPassenger businessPassenger;
+
+  @Before
+  public void initiateTest() {
+
+  }
+}
