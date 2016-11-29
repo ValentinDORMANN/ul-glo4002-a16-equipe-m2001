a1355b9db9990fe4eea4bf6525ec08c3d1e2444f
Hypolyte Tchoutouo <hypolyte.tchoutouo.1@ulaval.ca>
2016-11-29 01:03:21 -0500

    refactor luggage checked luggage and carryOnLuggage

--- flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/domain/luggage/CheckedLuggageTest.java
+++ flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/domain/luggage/CheckedLuggageTest.java
@@ -13,53 +13,34 @@ public class CheckedLuggageTest {
   private static final int OVER_DIMENSION_CM = 159;
   private static final int DIMENSION_LIMIT_CM = 158;
   private static final int ALLOWED_DIMENSION_CM = 157;
-  private static final String DIMENSION_UNIT_IN_CM = "cm";
-  private static final String WEIGHT_UNIT_IN_KG = "kg";
   private static final String LUGGAGE_TYPE = "checked";
   private static final String OTHER_LUGGAGE_TYPE = "other";
   private CheckedLuggage checkedLuggage;
 
   @Test(expected = NotAllowableLuggageException.class)
   public void givenLuggageWeightInKgOverLimitWhenCheckLuggageAllowableThenThrowException() {
-    checkedLuggage = new CheckedLuggage(ALLOWED_DIMENSION_CM, DIMENSION_UNIT_IN_CM, OVER_WEIGHT_KG, WEIGHT_UNIT_IN_KG);
+    checkedLuggage = new CheckedLuggage(ALLOWED_DIMENSION_CM, OVER_WEIGHT_KG);
 
-    checkedLuggage.checkLuggageAllowable();
+    checkedLuggage.verifyAllowableWeight(WEIGHT_LIMIT_KG);
   }
 
   @Test(expected = NotAllowableLuggageException.class)
   public void givenLuggageDimensionInCmOverLimitWhenCheckLuggageAllowableThenThrowException() {
-    checkedLuggage = new CheckedLuggage(OVER_DIMENSION_CM, DIMENSION_UNIT_IN_CM, ALLOWED_WEIGHT_KG, WEIGHT_UNIT_IN_KG);
+    checkedLuggage = new CheckedLuggage(OVER_DIMENSION_CM, ALLOWED_WEIGHT_KG);
 
-    checkedLuggage.checkLuggageAllowable();
-  }
-
-  @Test
-  public void givenLuggageAllowableWhenCheckLuggageAllowableThenDoNothing() {
-    checkedLuggage = new CheckedLuggage(ALLOWED_DIMENSION_CM, DIMENSION_UNIT_IN_CM, ALLOWED_WEIGHT_KG,
-        WEIGHT_UNIT_IN_KG);
-
-    checkedLuggage.checkLuggageAllowable();
-  }
-
-  @Test
-  public void givenLuggageWithLimitDimensionsWhenCheckLuggageAllowableThenDoNothing() {
-    checkedLuggage = new CheckedLuggage(DIMENSION_LIMIT_CM, DIMENSION_UNIT_IN_CM, WEIGHT_LIMIT_KG, WEIGHT_UNIT_IN_KG);
-
-    checkedLuggage.checkLuggageAllowable();
+    checkedLuggage.verifyAllowableDimension();
   }
 
   @Test
   public void givenCheckedLuggageWhenCompareIsTypeWithCheckedLuggageThenReturnTrue() {
-    checkedLuggage = new CheckedLuggage(ALLOWED_DIMENSION_CM, DIMENSION_UNIT_IN_CM, ALLOWED_WEIGHT_KG,
-        WEIGHT_UNIT_IN_KG);
+    checkedLuggage = new CheckedLuggage(ALLOWED_DIMENSION_CM, ALLOWED_WEIGHT_KG);
 
     assertTrue(checkedLuggage.isType(LUGGAGE_TYPE));
   }
 
   @Test
   public void givenCheckedLuggageWhenCompareIsTypeWithOtherLuggageThenReturnFalse() {
-    checkedLuggage = new CheckedLuggage(ALLOWED_DIMENSION_CM, DIMENSION_UNIT_IN_CM, ALLOWED_WEIGHT_KG,
-        WEIGHT_UNIT_IN_KG);
+    checkedLuggage = new CheckedLuggage(ALLOWED_DIMENSION_CM,ALLOWED_WEIGHT_KG);
 
     assertFalse(checkedLuggage.isType(OTHER_LUGGAGE_TYPE));
   }
