a6bee0f11d96945692b55700a4046b42950d9cdf
chagb <charly.agboton.1@ulaval.ca>
2016-12-05 01:25:26 -0500

    Story15 Done with Test

--- flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/services/seat/SeatAssignationLandScapeStrategyTest.java
+++ flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/services/seat/SeatAssignationLandScapeStrategyTest.java
@@ -1,6 +1,7 @@
 package ca.ulaval.glo4002.flycheckin.boarding.services.seat;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,35 +16,16 @@ public class SeatAssignationLandScapeStrategyTest {
 
   private static final String PASSENGER_SEAT_CLASS = "business";
   private static final String ANOTHER_SEAT_CLASS = "economic";
-  private static final String CHEAPER_BEST_VIEW_SEAT_NUMBER = "1A";
-  private static final String EXPENSIVE_BEST_VIEW_SEAT_NUMBER = "1D";
-  private static final String NEAR_WINDOW_SEAT_NUMBER = "2A";
-  private static final String CLEAR_VIEW_SEAT_NUMBER = "2C";
-  private static final String WORST_VIEW_SEAT_NUMBER = "3B";
-  private static final double CHEAPEST_PRICE = 200;
-  private static final double EXPENSIVE_PRICE = 300;
+  private static final String FIRST_SEAT_NUMBER = "1A";
+  private static final String SECOND_SEAT_NUMBER = "1D";
   private static final boolean IS_JUNIOR = true;
-  private static final int LEGROOM = 10;
 
-  private Seat anotherClassSeat, cheaperBestViewSeat, expensiveBestViewSeat, nearWindowSeat, clearViewSeat, worstViewSeat;
+  private Seat firstSeatMock, secondSeatMock;
   private List<Seat> availableSeats;
   private SeatAssignationLandScapeStrategy bestViewSeatAssignation;
 
   @Before
   public void initiateTest() {
-    cheaperBestViewSeat = new Seat(PASSENGER_SEAT_CLASS, CHEAPER_BEST_VIEW_SEAT_NUMBER, LEGROOM, true, true, false, CHEAPEST_PRICE);
-
-    expensiveBestViewSeat = new Seat(PASSENGER_SEAT_CLASS, EXPENSIVE_BEST_VIEW_SEAT_NUMBER, LEGROOM, true, true, false, EXPENSIVE_PRICE);
-
-    nearWindowSeat = new Seat(PASSENGER_SEAT_CLASS, NEAR_WINDOW_SEAT_NUMBER, LEGROOM, true, false, false, EXPENSIVE_PRICE);
-
-    clearViewSeat = new Seat(PASSENGER_SEAT_CLASS, CLEAR_VIEW_SEAT_NUMBER, LEGROOM, false, true, false, EXPENSIVE_PRICE);
-
-    worstViewSeat = new Seat(PASSENGER_SEAT_CLASS, WORST_VIEW_SEAT_NUMBER, LEGROOM, false, false, false, CHEAPEST_PRICE);
-
-    anotherClassSeat = new Seat();
-    anotherClassSeat.setSeatClass(ANOTHER_SEAT_CLASS);
-
     availableSeats = new ArrayList<Seat>();
     bestViewSeatAssignation = new SeatAssignationLandScapeStrategy();
   }
@@ -54,69 +36,142 @@ public class SeatAssignationLandScapeStrategyTest {
   }
 
   @Test(expected = NoSeatAvailableException.class)
-  public void givenSeatListWithNoOneHasPassengerSeatClassWhenAssignSeatToPassengerThenReturnException() {
-    availableSeats.add(anotherClassSeat);
+  public void givenOnlyOneSeatAvailableWithAnotherSeatClassWhenAssignSeatToPassengerThenReturnException() {
+    givenAnySeatAvailableWith(ANOTHER_SEAT_CLASS);
 
     bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, !IS_JUNIOR);
   }
 
   @Test
-  public void givenSeatWithoutClearViewAndWindowViewWhenAssignSeatToPassengerThenReturnSeat() {
-    availableSeats.add(worstViewSeat);
+  public void givenAnySeatAvailableWithPassengerSeatClassWhenAssignSeatToPassengerThenVerifySeatNumberIsAssigned() {
+    givenAnySeatAvailableWith(PASSENGER_SEAT_CLASS);
 
-    String assignedSeatNumber = bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, !IS_JUNIOR);
+    String seatNumber = bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, !IS_JUNIOR);
 
-    assertEquals(WORST_VIEW_SEAT_NUMBER, assignedSeatNumber);
+    assertEquals(FIRST_SEAT_NUMBER, seatNumber);
   }
 
   @Test
-  public void test2() {
-    availableSeats.add(worstViewSeat);
-    availableSeats.add(nearWindowSeat);
+  public void givenWorstViewSeatAndSeatWithWindowViewAvailableWhenAssignSeatToPassengerThenAssignWindowViewSeat() {
+    givenFirstSeatWithoutClearViewAndWindowView(PASSENGER_SEAT_CLASS);
+    givenSecondSeatWithOnlyWindowView(PASSENGER_SEAT_CLASS);
 
     String assignedSeatNumber = bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, !IS_JUNIOR);
 
-    assertEquals(NEAR_WINDOW_SEAT_NUMBER, assignedSeatNumber);
+    assertEquals(SECOND_SEAT_NUMBER, assignedSeatNumber);
   }
 
   @Test
-  public void test2b() {
-    availableSeats.add(worstViewSeat);
-    availableSeats.add(clearViewSeat);
+  public void givenWorstViewSeatAndSeatWithClearViewAvailableWhenAssignSeatToPassengerThenAssignClearViewSeat() {
+    givenFirstSeatWithoutClearViewAndWindowView(PASSENGER_SEAT_CLASS);
+    givenSecondSeatWithOnlyClearView(PASSENGER_SEAT_CLASS);
 
     String assignedSeatNumber = bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, !IS_JUNIOR);
 
-    assertEquals(CLEAR_VIEW_SEAT_NUMBER, assignedSeatNumber);
+    assertEquals(SECOND_SEAT_NUMBER, assignedSeatNumber);
   }
 
   @Test
-  public void test2c() {
-    availableSeats.add(clearViewSeat);
-    availableSeats.add(nearWindowSeat);
+  public void givenClearViewSeatAndWindowViewSeatAvailableWhenAssignSeatToPassengerThenAssignWindowViewSeat() {
+    givenFirstSeatWithOnlyClearView(PASSENGER_SEAT_CLASS);
+    givenSecondSeatWithOnlyWindowView(PASSENGER_SEAT_CLASS);
 
     String assignedSeatNumber = bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, !IS_JUNIOR);
 
-    assertEquals(NEAR_WINDOW_SEAT_NUMBER, assignedSeatNumber);
+    assertEquals(SECOND_SEAT_NUMBER, assignedSeatNumber);
   }
 
   @Test
-  public void test3() {
-    availableSeats.add(nearWindowSeat);
-    availableSeats.add(expensiveBestViewSeat);
+  public void givenBestViewSeatAndSeatWithOnlyWindowViewAvailableWhenAssignSeatToPassengerThenAssignBestViewSeat() {
+    givenFirstSeatWithOnlyClearView(PASSENGER_SEAT_CLASS);
+    when(firstSeatMock.isNearWindow()).thenReturn(true);
+    givenSecondSeatWithOnlyWindowView(PASSENGER_SEAT_CLASS);
 
     String assignedSeatNumber = bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, !IS_JUNIOR);
 
-    assertEquals(EXPENSIVE_BEST_VIEW_SEAT_NUMBER, assignedSeatNumber);
+    assertEquals(FIRST_SEAT_NUMBER, assignedSeatNumber);
   }
 
   @Test
-  public void test4() {
-    availableSeats.add(nearWindowSeat);
-    availableSeats.add(expensiveBestViewSeat);
-    availableSeats.add(cheaperBestViewSeat);
+  public void givenTwoSeatWithBothHasBestViewWhenAssignSeatToPassengerThenAssignTheCheapestSeat() {
+    givenFirstSeatWithOnlyClearView(PASSENGER_SEAT_CLASS);
+    when(firstSeatMock.isNearWindow()).thenReturn(true);
+    givenSecondSeatWithOnlyWindowView(PASSENGER_SEAT_CLASS);
+    when(secondSeatMock.isClearView()).thenReturn(true);
+    setSecondSeatCheaperThanFirstSeat();
 
     String assignedSeatNumber = bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, !IS_JUNIOR);
 
-    assertEquals(CHEAPER_BEST_VIEW_SEAT_NUMBER, assignedSeatNumber);
+    assertEquals(SECOND_SEAT_NUMBER, assignedSeatNumber);
+  }
+
+  @Test(expected = NoSeatAvailableException.class)
+  public void givenOnlyExitRowSeatAvailableWithPassengerSeatClassWhenAssignSeatToJuniorPassengerThenReturnException() {
+    givenAnyExitRowSeatAvailableWith(PASSENGER_SEAT_CLASS);
+
+    bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, IS_JUNIOR);
+  }
+
+  @Test
+  public void givenAnySeatNotInExitRowAndBestViewSeatInExitRowWhenAssignSeatToJuniorPassengerThenAssignSeatNotInExitRow() {
+    givenFirstSeatWithoutClearViewAndWindowView(PASSENGER_SEAT_CLASS);
+    givenSecondSeatWithOnlyWindowView(PASSENGER_SEAT_CLASS);
+    when(secondSeatMock.isClearView()).thenReturn(true);
+    when(secondSeatMock.isExitRow()).thenReturn(true);
+
+    String assignedSeatNumber = bestViewSeatAssignation.assignSeatNumber(availableSeats, PASSENGER_SEAT_CLASS, IS_JUNIOR);
+
+    assertEquals(FIRST_SEAT_NUMBER, assignedSeatNumber);
+  }
+
+  private void givenAnySeatAvailableWith(String seatClass) {
+    givenFirstSeat(seatClass);
+    availableSeats.add(firstSeatMock);
+  }
+
+  private void givenAnyExitRowSeatAvailableWith(String seatClass) {
+    givenFirstSeat(seatClass);
+    when(firstSeatMock.isExitRow()).thenReturn(true);
+    availableSeats.add(firstSeatMock);
+  }
+
+  private void givenFirstSeatWithoutClearViewAndWindowView(String seatClass) {
+    givenFirstSeat(seatClass);
+    availableSeats.add(firstSeatMock);
+  }
+
+  private void givenSecondSeatWithOnlyWindowView(String seatClass) {
+    givenSecondSeat(seatClass);
+    when(secondSeatMock.isNearWindow()).thenReturn(true);
+    availableSeats.add(secondSeatMock);
+  }
+
+  private void givenFirstSeatWithOnlyClearView(String seatClass) {
+    givenFirstSeat(seatClass);
+    when(firstSeatMock.isClearView()).thenReturn(true);
+    availableSeats.add(firstSeatMock);
+  }
+
+  private void givenSecondSeatWithOnlyClearView(String seatClass) {
+    givenSecondSeat(seatClass);
+    when(secondSeatMock.isClearView()).thenReturn(true);
+    availableSeats.add(secondSeatMock);
+  }
+
+  private void givenFirstSeat(String seatClass) {
+    firstSeatMock = mock(Seat.class);
+    when(firstSeatMock.getSeatNumber()).thenReturn(FIRST_SEAT_NUMBER);
+    when(firstSeatMock.hasClass(seatClass)).thenReturn(true);
+  }
+
+  private void givenSecondSeat(String seatClass) {
+    secondSeatMock = mock(Seat.class);
+    when(secondSeatMock.getSeatNumber()).thenReturn(SECOND_SEAT_NUMBER);
+    when(secondSeatMock.hasClass(seatClass)).thenReturn(true);
+  }
+
+  private void setSecondSeatCheaperThanFirstSeat() {
+    when(secondSeatMock.isCheaperThan(firstSeatMock)).thenReturn(true);
+    when(firstSeatMock.isCheaperThan(secondSeatMock)).thenReturn(false);
   }
 }
