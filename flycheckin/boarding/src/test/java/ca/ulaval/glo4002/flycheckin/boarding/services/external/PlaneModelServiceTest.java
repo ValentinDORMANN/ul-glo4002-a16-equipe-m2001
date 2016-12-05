68de7c7ba92006067e13e652ca99f3f105303412
virgil <virgil@debian>
2016-12-04 19:51:30 -0500

    refactor plane model test

--- flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/services/external/PlaneModelServiceTest.java
+++ flycheckin/boarding/src/test/java/ca/ulaval/glo4002/flycheckin/boarding/services/external/PlaneModelServiceTest.java
@@ -17,50 +17,76 @@ import ca.ulaval.glo4002.flycheckin.boarding.rest.dto.SeatDto;
 
 public class PlaneModelServiceTest {
 
-  private static final int[] EXIT_ROWS = { 1 };
-  private static final int ROW = 1;
+  private static final int EXIT_ROW = 1;
+  private static final int ANOTHER_ROW = 2;
   private static final int LEGROOM = 56;
   private static final String SEAT = "A";
   private static final boolean WINDOW = true;
   private static final boolean CLEAR_VIEW = true;
   private static final double PRICE = 100;
   private static final String CLASS_NAME = "economic";
-  private static final int[] ROWS = { ROW };
+  private static final int[] EXIT_ROWS = { EXIT_ROW };
+  private static final int[] ROWS = { EXIT_ROW, ANOTHER_ROW };
   private static final String PLANE_MODEL = "a320";
-  private PlaneModelHttpClient mockPlaneModelHttpClient;
+
+  private PlaneModelHttpClient planeModelHttpClientMock;
   private PlaneModelDto planeModelDto;
-  private SeatDto mockSeatDto;
-  private ClassPassengerDto mockClassPassengerDto;
+  private SeatDto seatDto;
+  private ClassPassengerDto classPassengerDto;
   private PlaneModelService planeModelService;
 
   @Before
   public void initiateTest() {
-    mockPlaneModelHttpClient = mock(PlaneModelHttpClient.class);
+    planeModelHttpClientMock = mock(PlaneModelHttpClient.class);
     planeModelDto = new PlaneModelDto();
-    mockSeatDto = mock(SeatDto.class);
-    mockClassPassengerDto = mock(ClassPassengerDto.class);
-    planeModelService = new PlaneModelService(mockPlaneModelHttpClient);
+    seatDto = new SeatDto();
+    classPassengerDto = new ClassPassengerDto();
+
+    planeModelService = new PlaneModelService(planeModelHttpClientMock);
   }
 
   @Test
   public void givenPlaneModelWhenGetPlaneSeatsThenReturnNotEmptySeatList() {
-    mockSeatDto.row = ROW;
-    mockSeatDto.seat = SEAT;
-    mockSeatDto.legroom = LEGROOM;
-    mockSeatDto.window = WINDOW;
-    mockSeatDto.clear_view = CLEAR_VIEW;
-    mockSeatDto.price = PRICE;
-    mockClassPassengerDto.rows = ROWS;
-    mockClassPassengerDto.name = CLASS_NAME;
-    SeatDto[] seatsDto = { mockSeatDto };
-    planeModelDto.seats = seatsDto;
-    ClassPassengerDto[] classesDto = { mockClassPassengerDto };
-    planeModelDto.classes = classesDto;
-    planeModelDto.exit_rows = EXIT_ROWS;
-    willReturn(planeModelDto).given(mockPlaneModelHttpClient).getPlaneModelDtoAccordingPlaneModel(PLANE_MODEL);
+    givenPlaneModel();
 
     List<Seat> seats = planeModelService.getSeatsAccordingPlaneModel(PLANE_MODEL);
 
     assertFalse(seats.isEmpty());
+  }
+
+  @Test
+  public void givenSeatInExitRowWhenGetPlaneSeatsThenReturnTrue() {
+    givenPlaneModel();
+
+    List<Seat> seats = planeModelService.getSeatsAccordingPlaneModel(PLANE_MODEL);
+
+    assertTrue(seats.get(0).isExitRow());
+  }
+
+  @Test
+  public void givenSeatNotInExitRowWhenGetPlaneSeatsThenReturnFalse() {
+    givenPlaneModel();
+    seatDto.row = ANOTHER_ROW;
+
+    List<Seat> seats = planeModelService.getSeatsAccordingPlaneModel(PLANE_MODEL);
+
+    assertFalse(seats.get(0).isExitRow());
+  }
+
+  private void givenPlaneModel() {
+    seatDto.row = EXIT_ROW;
+    seatDto.seat = SEAT;
+    seatDto.legroom = LEGROOM;
+    seatDto.window = WINDOW;
+    seatDto.clear_view = CLEAR_VIEW;
+    seatDto.price = PRICE;
+    classPassengerDto.rows = ROWS;
+    classPassengerDto.name = CLASS_NAME;
+    SeatDto[] seatsDto = { seatDto };
+    planeModelDto.seats = seatsDto;
+    ClassPassengerDto[] classesDto = { classPassengerDto };
+    planeModelDto.classes = classesDto;
+    planeModelDto.exit_rows = EXIT_ROWS;
+    willReturn(planeModelDto).given(planeModelHttpClientMock).getPlaneModelDtoAccordingPlaneModel(PLANE_MODEL);
   }
 }
