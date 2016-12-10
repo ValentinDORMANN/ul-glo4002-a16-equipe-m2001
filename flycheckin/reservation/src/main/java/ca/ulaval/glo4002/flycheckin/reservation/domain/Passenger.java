043561ae851ecac25f89f042b2364157717be673
Hypolyte Tchoutouo <hypolyte.tchoutouo.1@ulaval.ca>
2016-12-10 01:28:32 -0500

    refactor hibernate

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/Passenger.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/Passenger.java
@@ -1,71 +1,90 @@
 package ca.ulaval.glo4002.flycheckin.reservation.domain;
 
 import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import ca.ulaval.glo4002.flycheckin.reservation.rest.dto.PassengerDto;
 
+@Entity
 public class Passenger {
 
-  private static final String EMPTY = "";
-  private static final int CHILD_AGE = 21;
-  private String passengerHash;
-  private String firstName;
-  private String lastName;
-  private int age;
-  private boolean isVip;
-  private String passportNumber;
-  private String seatClass;
+	private static final String EMPTY = "";
+	private static final int CHILD_AGE = 21;
 
-  public Passenger(PassengerDto passengerDto) throws IllegalArgumentException {
-    this.firstName = passengerDto.first_name;
-    this.lastName = passengerDto.last_name;
-    this.age = passengerDto.age;
-    this.passportNumber = passengerDto.passport_number;
-    this.seatClass = passengerDto.seat_class;
-    this.passengerHash = UUID.randomUUID().toString();
-  }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String passengerHash;
+	@Column(name = "firstName")
+	private String firstName;
+	@Column(name = "lastName")
+	private String lastName;
+	@Column(name = "age")
+	private int age;
+	@Column(name = "passeportNumber")
+	private String passportNumber;
+	@Column(name = "seatClass")
+	private String seatClass;
+	@Column(name = "isvip")
+	private boolean isVip;
 
-  public boolean isValid() {
-    return !(firstName.trim().equals(EMPTY) || lastName.trim().equals(EMPTY) || passportNumber.trim().equals(EMPTY));
-  }
+	public Passenger() {
+	}
 
-  public boolean hasThisHash(String passengerHash) {
-    return this.passengerHash.equals(passengerHash);
-  }
+	public Passenger(PassengerDto passengerDto) throws IllegalArgumentException {
+		this.firstName = passengerDto.first_name;
+		this.lastName = passengerDto.last_name;
+		this.age = passengerDto.age;
+		this.passportNumber = passengerDto.passport_number;
+		this.seatClass = passengerDto.seat_class;
+		this.passengerHash = UUID.randomUUID().toString();
+	}
 
-  public boolean isChild() {
-    return age < CHILD_AGE;
-  }
+	public boolean isValid() {
+		return !(firstName.trim().equals(EMPTY) || lastName.trim().equals(EMPTY)
+				|| passportNumber.trim().equals(EMPTY));
+	}
 
-  public void changeVipStatus(boolean isVip) {
-    this.isVip = isVip;
-  }
+	public boolean hasThisHash(String passengerHash) {
+		return this.passengerHash.equals(passengerHash);
+	}
 
-  public int getAge() {
-    return age;
-  }
+	public boolean isChild() {
+		return age < CHILD_AGE;
+	}
 
-  public String getPassengerHash() {
-    return passengerHash;
-  }
+	public void changeVipStatus(boolean isVip) {
+		this.isVip = isVip;
+	}
 
-  public String getFirstName() {
-    return firstName;
-  }
+	public int getAge() {
+		return age;
+	}
 
-  public String getLastName() {
-    return lastName;
-  }
+	public String getPassengerHash() {
+		return passengerHash;
+	}
 
-  public String getPassportNumber() {
-    return passportNumber;
-  }
+	public String getFirstName() {
+		return firstName;
+	}
 
-  public String getSeatClass() {
-    return seatClass;
-  }
+	public String getLastName() {
+		return lastName;
+	}
 
-  public boolean getIsVip() {
-    return isVip;
-  }
+	public String getPassportNumber() {
+		return passportNumber;
+	}
+
+	public String getSeatClass() {
+		return seatClass;
+	}
+
+	public boolean getIsVip() {
+		return isVip;
+	}
 }
---
+++ flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/persistence/HibernateCheckinTest.java
@@ -0,0 +1,11 @@
+package ca.ulaval.glo4002.flycheckin.reservation.persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HibernateCheckinTest {
+
+	
+
+}
