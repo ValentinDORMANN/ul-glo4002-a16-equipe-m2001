48902d7f321e36ef07c0089f25f278044d03fed8
chagb <charly.agboton.1@ulaval.ca>
2016-12-11 19:56:27 -0500

    Refactor LuggageFactory

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/luggage/LuggageFactory.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/domain/luggage/LuggageFactory.java
@@ -2,17 +2,18 @@ package ca.ulaval.glo4002.flycheckin.boarding.domain.luggage;
 
 public class LuggageFactory {
 
-  private static final String CHECKED_LUGGAGE = "checked";
+  private static final String REGULAR_LUGGAGE = "checked";
   private static final String CARRY_ON_LUGGAGE = "carry-on";
+  private static final String CARRY_ON_CATEGORY = "standard";
 
   public Luggage createLuggage(int linearDimension, int weight, String type) {
     Luggage luggage = null;
     switch (type) {
-      case CHECKED_LUGGAGE:
-        luggage = new CheckedLuggage(linearDimension, weight);
+      case REGULAR_LUGGAGE:
+        luggage = new CheckedLuggage(linearDimension, weight, REGULAR_LUGGAGE);
         break;
       case CARRY_ON_LUGGAGE:
-        luggage = new CarryOnLuggage(linearDimension, weight);
+        luggage = new CarryOnLuggage(linearDimension, weight, CARRY_ON_CATEGORY);
         break;
       default:
         throw new UndefinedTypeLuggageException();
