caf366b2003701958bd1914b84fef73410f76a46
Hypolyte Tchoutouo <hypolyte.tchoutouo.1@ulaval.ca>
2016-12-16 15:03:57 -0500

    refactor hibernate

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/CheckinService.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/CheckinService.java
@@ -1,4 +1,5 @@
 package ca.ulaval.glo4002.flycheckin.reservation.domain;
+
 import ca.ulaval.glo4002.flycheckin.reservation.exception.ReservationModuleException;
 import ca.ulaval.glo4002.flycheckin.reservation.persistence.CheckinInMemory;
 import ca.ulaval.glo4002.flycheckin.reservation.persistence.HibernateCheckin;
@@ -8,29 +9,33 @@ import ca.ulaval.glo4002.flycheckin.reservation.rest.dto.CheckinDto;
 
 public class CheckinService {
 
-  private static final String MESSAGE_ERROR = "Passenger Information incorrect";
-  private CheckinInMemory checkinInMemory;
- // private ReservationInMemory reservationInMemory;
-  private HibernateReservation hibernateReservation;
-  private HibernateCheckin hibernateCheckin;
-  private Reservation reservation;
+	private static final String MESSAGE_ERROR = "Passenger Information incorrect";
+	private CheckinInMemory checkinInMemory;
+	// private ReservationInMemory reservationInMemory;
+	private HibernateReservation hibernateReservation;
+	private HibernateCheckin hibernateCheckin;
+	
 
-  public CheckinService(CheckinInMemory checkinInMemory, HibernateReservation hibernateReservation) {
-    this.checkinInMemory = checkinInMemory;
-    //this.reservationInMemory = reservationInMemory;
-    this.hibernateReservation=hibernateReservation;
-  }
+	public CheckinService() {
+		this.checkinInMemory = checkinInMemory;
+		this.hibernateReservation = hibernateReservation;
+	}
 
-  public int saveCheckin(CheckinDto checkinDto) throws ReservationModuleException {
-    String hash = checkinDto.passenger_hash;
-    String by = checkinDto.by;
-    reservation = hibernateReservation.findReservationByPassengerHash(hash);
-    reservation.validateCheckinPeriod(by);
-    if (reservation.isPassengerInfosValid(hash)){
-    	//CheckIn checkin = new CheckIn(checkinDto);
-    	//hibernateCheckin.persistCheckIn(checkin);
-      return checkinInMemory.doPassengerCheckin(hash);
-    }
-    throw new ReservationModuleException(MESSAGE_ERROR);
-  }
+	public CheckinService(CheckinInMemory checkinInMemory, HibernateReservation hibernateReservation) {
+		this.checkinInMemory = checkinInMemory;
+		this.hibernateReservation = hibernateReservation;
+	}
+
+	public int saveCheckin(CheckinDto checkinDto) throws ReservationModuleException {
+		String hash = checkinDto.passenger_hash;
+		String by = checkinDto.by;
+		Reservation reservation = hibernateReservation.findReservationByPassengerHash(hash);
+		reservation.validateCheckinPeriod(by);
+		if (reservation.isPassengerInfosValid(hash)) {
+			// CheckIn checkin = new CheckIn(checkinDto);
+			// hibernateCheckin.persistCheckIn(checkin);
+			return checkinInMemory.doPassengerCheckin(hash);
+		}
+		throw new ReservationModuleException(MESSAGE_ERROR);
+	}
 }
--- flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/domain/CheckinServiceTest.java
+++ flycheckin/reservation/src/test/java/ca/ulaval/glo4002/flycheckin/reservation/domain/CheckinServiceTest.java
@@ -11,70 +11,78 @@ import ca.ulaval.glo4002.flycheckin.reservation.exception.NotTimeToCheckinExcept
 import ca.ulaval.glo4002.flycheckin.reservation.persistence.CheckinInMemory;
 import ca.ulaval.glo4002.flycheckin.reservation.persistence.HibernateCheckin;
 import ca.ulaval.glo4002.flycheckin.reservation.persistence.HibernateReservation;
-import ca.ulaval.glo4002.flycheckin.reservation.persistence.ReservationInMemory;
 import ca.ulaval.glo4002.flycheckin.reservation.rest.dto.CheckinDto;
 
 public class CheckinServiceTest {
 
-  private static final String PASSENGER_HASH = "HASH";
-  private static final String FAKE_PASSENGER_HASH = "FAKE_HASH";
-  private static final String AGENT = "AGENT";
-  private static final String SELF = "SELF";
-  private static final int CHECKIN_NUMBER = 1;
-  private static final boolean IS_VALID = true;
-  private static final boolean IS_NOT_VALID = false;
-  private CheckinInMemory mockCheckinInMemory;//go
-  private HibernateCheckin hibernateCheckinMock;
-  private ReservationInMemory mockReservationInMemory;//go
-  private HibernateReservation hibernateReservationMock;
-  private CheckinDto CheckinDto;
-  private Reservation reservationMock;
-  private CheckinService checkinService;
+	private static final String PASSENGER_HASH = "HASH";
+	private static final String FAKE_PASSENGER_HASH = "FAKE_HASH";
+	private static final String AGENT = "AGENT";
+	private static final String SELF = "SELF";
+	private static final int CHECKIN_NUMBER = 1;
+	private static final boolean IS_VALID = true;
+	private static final boolean IS_NOT_VALID = false;
+	private CheckinInMemory mockCheckinInMemory;// go
+	private HibernateCheckin hibernateCheckinMock;
+	private HibernateReservation hibernateReservationMock;
+	private CheckinDto checkinDto;
+	private Reservation reservationMock;
+	private CheckinService checkinService;
 
-  @Before
-  public void initiateTest() {
-	  CheckinDto = new CheckinDto();
-    mockCheckinInMemory = mock(CheckinInMemory.class);//go
-    hibernateReservationMock = mock(HibernateReservation.class);//go
-    reservationMock = mock(Reservation.class);
-    CheckinDto.passenger_hash = PASSENGER_HASH;
-    CheckinDto.by = AGENT;
-    willReturn(CHECKIN_NUMBER).given(mockCheckinInMemory).doPassengerCheckin(PASSENGER_HASH);
-    checkinService = new CheckinService(mockCheckinInMemory, hibernateReservationMock);
-  }
+	@Before
+	public void initiateTest() {
+		checkinDto = new CheckinDto();
+		mockCheckinInMemory = mock(CheckinInMemory.class);// go
+		hibernateReservationMock = mock(HibernateReservation.class);// go
+		reservationMock = mock(Reservation.class);
+		assignElementToDto( checkinDto);
+		driveMockObject(mockCheckinInMemory, hibernateReservationMock, reservationMock);
 
-  @Test(expected = NotFoundPassengerException.class)
-  public void givenFakePassengerWhenAgentCheckinThenThrowException() {
-	  willThrow(NotFoundPassengerException.class).given(hibernateReservationMock)
-      .findReservationByPassengerHash(FAKE_PASSENGER_HASH);
-	  CheckinDto.passenger_hash = FAKE_PASSENGER_HASH;
+		checkinService = new CheckinService(mockCheckinInMemory, hibernateReservationMock);
+	}
 
-    checkinService.saveCheckin(CheckinDto);
-  }
+	@Test(expected = NotFoundPassengerException.class)
+	public void givenFakePassengerWhenAgentCheckinThenThrowException() {
+		willThrow(NotFoundPassengerException.class).given(hibernateReservationMock)
+				.findReservationByPassengerHash(FAKE_PASSENGER_HASH);
+		checkinDto.passenger_hash = FAKE_PASSENGER_HASH;
 
-  @Test(expected = NotTimeToCheckinException.class)
-  public void givenPassengerWhenSelfCheckinNotInTimeThenThrowException() {
-	  willReturn(reservationMock).given(hibernateReservationMock).findReservationByPassengerHash(PASSENGER_HASH);
-	  CheckinDto.by = SELF;
-    willThrow(NotTimeToCheckinException.class).given(reservationMock).validateCheckinPeriod(CheckinDto.by);
+		checkinService.saveCheckin(checkinDto);
+	}
 
-    checkinService.saveCheckin(CheckinDto);
-  }
+	@Test(expected = NotTimeToCheckinException.class)
+	public void givenPassengerWhenSelfCheckinNotInTimeThenThrowException() {
+		checkinDto.by = SELF;
+		willThrow(NotTimeToCheckinException.class).given(reservationMock).validateCheckinPeriod(checkinDto.by);
 
-  @Test(expected = ReservationModuleException.class)
-  public void givenWrongPassengerInformationWhenCheckinThenThrowException() {
-	  willReturn(reservationMock).given(hibernateReservationMock).findReservationByPassengerHash(PASSENGER_HASH);
-    willReturn(IS_NOT_VALID).given(reservationMock).isPassengerInfosValid(PASSENGER_HASH);
+		checkinService.saveCheckin(checkinDto);
+	}
 
-    checkinService.saveCheckin(CheckinDto);
-  }
+	@Test(expected = ReservationModuleException.class)
+	public void givenWrongPassengerInformationWhenCheckinThenThrowException() {
+	
+		willReturn(IS_NOT_VALID).given(reservationMock).isPassengerInfosValid(PASSENGER_HASH);
 
-  @Test
-  public void givenValidPassengerWhenDoCheckinThenReturnCheckinNumber() {
-    willReturn(IS_VALID).given(reservationMock).isPassengerInfosValid(PASSENGER_HASH);
+		checkinService.saveCheckin(checkinDto);
+	}
 
-    int checkinNumber = checkinService.saveCheckin(CheckinDto);
+	@Test
+	public void givenValidPassengerWhenDoCheckinThenReturnCheckinNumber() {
 
-    assertEquals(CHECKIN_NUMBER, checkinNumber);
-  }
+		int checkinNumber = checkinService.saveCheckin(checkinDto);
+
+		assertEquals(CHECKIN_NUMBER, checkinNumber);
+	}
+
+	private void driveMockObject(CheckinInMemory mockCheckinInMemory, HibernateReservation hibernateReservationMock,
+			Reservation reservationMock) {
+		willReturn(reservationMock).given(hibernateReservationMock).findReservationByPassengerHash(PASSENGER_HASH);
+		willReturn(IS_VALID).given(reservationMock).isPassengerInfosValid(PASSENGER_HASH);
+		willReturn(CHECKIN_NUMBER).given(mockCheckinInMemory).doPassengerCheckin(PASSENGER_HASH);
+	}
+	
+	private void assignElementToDto(CheckinDto checkinDto){
+		checkinDto.passenger_hash = PASSENGER_HASH;
+		checkinDto.by = AGENT;
+	}
 }
