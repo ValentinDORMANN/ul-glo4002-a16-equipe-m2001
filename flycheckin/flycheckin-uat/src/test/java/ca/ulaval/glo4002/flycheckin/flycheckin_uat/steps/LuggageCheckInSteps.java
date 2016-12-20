98bc10579b54decbe9ff444323d2d06c1ec9ad3e
chagb <charly.agboton.1@ulaval.ca>
2016-12-20 07:35:48 -0500

    Scenario 1 done

--- flycheckin/flycheckin-uat/src/test/java/ca/ulaval/glo4002/flycheckin/flycheckin_uat/steps/LuggageCheckInSteps.java
+++ flycheckin/flycheckin-uat/src/test/java/ca/ulaval/glo4002/flycheckin/flycheckin_uat/steps/LuggageCheckInSteps.java
@@ -1,48 +1,106 @@
 package ca.ulaval.glo4002.flycheckin.flycheckin_uat.steps;
 
-import ca.ulaval.glo4002.flycheckin.reservation.domain.Passenger;
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import ca.ulaval.glo4002.flycheckin.boarding.domain.luggage.Luggage;
+import ca.ulaval.glo4002.flycheckin.boarding.domain.luggage.LuggageFactory;
+import ca.ulaval.glo4002.flycheckin.boarding.domain.passenger.Passenger;
+import ca.ulaval.glo4002.flycheckin.boarding.domain.passenger.PassengerFactory;
+import ca.ulaval.glo4002.flycheckin.reservation.domain.Reservation;
 import ca.ulaval.glo4002.flycheckin.reservation.rest.dto.PassengerDto;
-import cucumber.api.PendingException;
+import ca.ulaval.glo4002.flycheckin.reservation.rest.dto.ReservationDto;
 import cucumber.api.java8.En;
 
 public class LuggageCheckInSteps implements En {
-  private static final String LAST_NAME = "mary";
-  private static final String FIRST_NAME = "john";
+  private static final String LAST_NAME = "Martin";
   private static final int AGE = 23;
   private static final String PASSPORT_NUMBER = "AB123";
-  private static final String SEAT_CLASS = "economic";
-  PassengerDto passengerDto;
+  private static final int RESERVATION_NUMBER = 37999;
+  private static final String RESERVATION_DATE = "2016-01-31";
+  private static final String RESERVATION_CONFIRMATION = "12345";
+  private static final String FLIGHT_DATE = "2016-10-30T00:00:00Z";
+  private static final String PAYEMENT_LOCATION = "/payments/daghkjhg";
+  private static final SimpleDateFormat DATE_FORMAT_COURT = new SimpleDateFormat("yyyy-MM-dd");
+  private static final SimpleDateFormat DATE_FORMAT_LONG = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+  PassengerDto passengerDto = new PassengerDto();
+  ReservationDto reservationDto = new ReservationDto();
+  Passenger passenger;
+  Reservation reservation;
 
   @cucumber.api.java.Before
-  public void beforeScenario() {
-    passengerDto = new PassengerDto();
+  public void beforeScenario() throws ParseException {
+    Date reservationDate = DATE_FORMAT_COURT.parse(RESERVATION_DATE);
+    Date flightDate = DATE_FORMAT_LONG.parse(FLIGHT_DATE);
+
+    reservationDto.reservation_number = RESERVATION_NUMBER;
+    reservationDto.reservation_date = reservationDate;
+    reservationDto.reservation_confirmation = RESERVATION_CONFIRMATION;
+    reservationDto.flight_date = flightDate;
+    reservationDto.payment_location = PAYEMENT_LOCATION;
+
     passengerDto.last_name = LAST_NAME;
-    passengerDto.first_name = FIRST_NAME;
     passengerDto.age = AGE;
     passengerDto.passport_number = PASSPORT_NUMBER;
-    passengerDto.seat_class = SEAT_CLASS;
   }
 
   public LuggageCheckInSteps() {
-    Given("^a passenger Bob with an economy class reservation on flight AC-(\\d+)$", (String flightNumber) -> {
-      Passenger bob = new Passenger(passengerDto);
-      throw new PendingException();
+    Given("^a passenger \"([^\"]*)\" with an \"([^\"]*)\" class reservation on flight \"([^\"]*)\"$",
+        (String name, String seatClass, String flightNumber) -> {
+          reservation = createPassengerBob(name, seatClass, flightNumber);
+        });
+
+    Given("^already has a \"([^\"]*)\" luggage meeting the standards$", (String type) -> {
+      addStandardLuggage(reservation, type);
     });
 
-    Given("^already has a checked luggage meeting the standards$", () -> {
-      // Write code here that turns the phrase above into concrete actions
-      throw new PendingException();
+    When("^He registers another \"([^\"]*)\" luggage weighing (\\d+)kg and size (\\d+)cm$", (String type, Integer weight, Integer size) -> {
+      addSpecificLuggage(size, weight, type);
     });
 
-    When("^He registers another checked luggage weighing (\\d+)kg and size (\\d+)cm$", (Integer weight, Integer size) -> {
-      // Write code here that turns the phrase above into concrete actions
-      throw new PendingException();
+    Then("^the amount to pay is (\\d+)\\$$", (Double amount) -> {
+      assertEquals(amount, passenger.getTotalPrice(), 0.001);
     });
 
-    Then("^the amount to pay is (\\d+)\\$$", (Float amount) -> {
-      // Write code here that turns the phrase above into concrete actions
-      throw new PendingException();
-    });
+  }
 
+  public Reservation createPassengerBob(String name, String seatClass, String flightNumber) {
+    reservationDto.flight_number = flightNumber;
+    passengerDto.first_name = name;
+    passengerDto.seat_class = seatClass;
+    reservationDto.passengers = new ArrayList<PassengerDto>();
+    reservationDto.passengers.add(passengerDto);
+    reservation = new Reservation(reservationDto);
+    return reservation;
+  }
+
+  public void addStandardLuggage(Reservation reservation, String type) {
+    passenger = createBoardingPassenger(reservation);
+    int standardSize = 150;
+    int standardWeight = 22;
+    Luggage luggage = createLuggage(standardSize, standardWeight, type);
+    passenger.addLuggage(luggage);
+  }
+
+  public void addSpecificLuggage(int size, int weight, String type) {
+    Luggage luggage = createLuggage(size, weight, type);
+    passenger.addLuggage(luggage);
+  }
+
+  private Passenger createBoardingPassenger(Reservation reservation) {
+    PassengerFactory passengerFactory = new PassengerFactory();
+    String passengerHash = reservation.getPassengers().get(0).getPassengerHash();
+    String seatClass = reservation.getPassengers().get(0).getSeatClass();
+    return passengerFactory.createPassenger(reservation.getFlightNumber(), reservation.getFlightDate(), passengerHash, seatClass, false,
+        false);
+  }
+
+  private Luggage createLuggage(int size, int weight, String type) {
+    LuggageFactory luggageFactory = new LuggageFactory();
+    return luggageFactory.createLuggage(size, weight, type);
   }
 }
--- flycheckin/flycheckin-uat/src/test/resources/flycheckin_uat/luggage_checkin.feature
+++ flycheckin/flycheckin-uat/src/test/resources/flycheckin_uat/luggage_checkin.feature
@@ -3,8 +3,12 @@ Feature: Luggage Check-in
     As a flight passenger
     I want to be able to add a luggage 
     
-Scenario: Adding a luggage adjust luggages fees
-  Given a passenger Bob with an economy class reservation on flight AC-1234
-  And already has a checked luggage meeting the standards
-  When He registers another checked luggage weighing 10kg and size 100cm
+Scenario Outline: Adding a luggage adjust luggages fees
+  Given a passenger "<name>" with an "<seatClass>" class reservation on flight "<flightNumber>"
+  And already has a "<type>" luggage meeting the standards
+  When He registers another "<type>" luggage weighing 10kg and size 100cm
   Then the amount to pay is 50$
+  
+  Examples:
+  |name|seatClass|flightNumber|type|
+  |Bob |economy  |AC-1234     |checked|
