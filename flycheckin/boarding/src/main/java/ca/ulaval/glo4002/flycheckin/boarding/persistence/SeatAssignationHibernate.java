27834e1630b2cca02855d2c2aae04e8e989fe0e9
virgil <virgil@debian>
2016-12-20 05:50:05 -0500

    refactor seat assignation

--- flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/persistence/SeatAssignationHibernate.java
+++ flycheckin/boarding/src/main/java/ca/ulaval/glo4002/flycheckin/boarding/persistence/SeatAssignationHibernate.java
@@ -2,27 +2,26 @@ package ca.ulaval.glo4002.flycheckin.boarding.persistence;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import javax.persistence.TypedQuery;
 
 import ca.ulaval.glo4002.flycheckin.boarding.domain.seat.SeatAssignation;
 import ca.ulaval.glo4002.flycheckin.boarding.domain.seat.SeatAssignationRepository;
 
-public class SeatAssignationHibernate implements SeatAssignationRepository{
+public class SeatAssignationHibernate implements SeatAssignationRepository {
 
   private static final String EMPTY_STRING = "";
   private static final String ERROR_SEAT_UNASSIGNED = "Error: This passenger seat is already assigned.";
   private static final String ERROR_ASSIGNATION_NUMBER_USED = "Error: This assignation number is already used.";
-  
+
   private EntityManager entityManager;
-  
+
   public SeatAssignationHibernate() {
     this.entityManager = new EntityManagerProvider().getEntityManager();
   }
-  
+
   public SeatAssignationHibernate(EntityManager entityManager) {
     this.entityManager = entityManager;
   }
-  
+
   @Override
   public void persistSeatAssignation(SeatAssignation seatAssignation) {
     EntityTransaction transaction = entityManager.getTransaction();
@@ -30,7 +29,7 @@ public class SeatAssignationHibernate implements SeatAssignationRepository{
     try {
       if (entityManager.find(SeatAssignation.class, seatAssignation.getAssignationNumber()) != null)
         throw new AssignationNumberUsedException(ERROR_ASSIGNATION_NUMBER_USED);
-      else if (!getPassengerHashSeatNumber(seatAssignation.getPassengerHash()).isEmpty())
+      else if (isThisSeatAlreadyAssigned(seatAssignation.getPassengerHash()))
         throw new SeatAlreadyAssignedException(ERROR_SEAT_UNASSIGNED);
       entityManager.persist(seatAssignation);
     } finally {
@@ -38,19 +37,7 @@ public class SeatAssignationHibernate implements SeatAssignationRepository{
     }
   }
 
-  @Override
-  public String getPassengerHashSeatNumber(String passengerHash) {
-    String seatNumber = EMPTY_STRING;
-    SeatAssignation seatAssignationFound;
-    String hql = "select s from SeatAssignation s where s.passengerHash = :passengerHash";
-    
-    TypedQuery<SeatAssignation> query = entityManager.createQuery(hql, SeatAssignation.class);
-    query.setParameter("passengerHash", passengerHash);
-    seatAssignationFound = query.getSingleResult();
-    seatAssignationFound = entityManager.find(SeatAssignation.class, passengerHash);
-    
-    if (seatAssignationFound == null)
-      return seatNumber;
-    return seatAssignationFound.getSeatNumber();
+  private boolean isThisSeatAlreadyAssigned(String passengerHash) {
+    return entityManager.find(SeatAssignation.class, passengerHash) != null;
   }
 }
