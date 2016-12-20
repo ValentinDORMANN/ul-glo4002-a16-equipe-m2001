7da0599c7f030e67f56970774ee067d83dca7a97
ValentinDORMANN <valentin.dormann@gmail.com>
2016-12-20 01:21:07 -0500

    change attribut visibility to private and checkstyle

--- flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/CheckIn.java
+++ flycheckin/reservation/src/main/java/ca/ulaval/glo4002/flycheckin/reservation/domain/CheckIn.java
@@ -8,43 +8,39 @@ import javax.persistence.Id;
 import ca.ulaval.glo4002.flycheckin.reservation.rest.dto.CheckinDto;
 
 public class CheckIn {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	public int id;
-	@Column(name = "passengerHash")
-	private String passengerHash;
-	@Column(name = "by")
-	private String by;
-	@Column(name = "isvip")
-	private boolean isVip;
-	
-	public CheckIn(){
-		
-	}
-	
-	public CheckIn(CheckinDto checkinDto){
-		this.passengerHash = checkinDto.passenger_hash;
-		this.by = checkinDto.by;
-		this.isVip = checkinDto.vip;
-		
-	}
-	
-	public int getId(){
-		return id;
-	}
-	
-	public String getPassengerHash(){
-		return passengerHash;
-	}
-	
-	public String getCheckedBy(){
-		return by;
-	}
-	
-	public boolean getStatus(){
-		return isVip;
-	}
-	
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+  @Column(name = "passengerHash")
+  private String passengerHash;
+  @Column(name = "by")
+  private String by;
+  @Column(name = "isvip")
+  private boolean isVip;
+
+  public CheckIn(){
+  }
+
+  public CheckIn(CheckinDto checkinDto){
+    this.passengerHash = checkinDto.passenger_hash;
+    this.by = checkinDto.by;
+    this.isVip = checkinDto.vip;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public String getPassengerHash(){
+    return passengerHash;
+  }
+
+  public String getCheckedBy(){
+    return by;
+  }
+
+  public boolean getStatus(){
+    return isVip;
+  }
 }
